28612839a93db726e55cd92a5ea63eee882fc953
Alasdair McLeay <alasdair.mcleay@me.com>
2017-10-25 18:55:01 +0100

    readme, example component, remove redundant create-react-app content

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,10 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Example } from '..';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('Example', module).add('Default', () => <Example />);
+storiesOf('Example', module).add('With child text', () => <Example>Example</Example>);
