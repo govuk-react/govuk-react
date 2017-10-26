79829f77fc0a23b6faa1bd19fc8cba4cbeceb0b5
Alasdair McLeay <alasdair.mcleay@me.com>
2017-10-26 23:49:58 +0100

    constants

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export * as colours from "./colours"
+
+export const BORDER_WIDTH_MOBILE = "4px";
+export const BORDER_WIDTH_TABLET = "5px";
+export const BORDER_WIDTH_FORM_ELEMENT = "2px";
+export const BORDER_WIDTH_ERROR = "4px";
+export const BORDER_WIDTH = "5px";
+export const BORDER_WIDTH_WIDE = "10px";
+export const FOCUS_WIDTH = "3px";
+export const SITE_WIDTH = "960px";
+export const FULL_WIDTH = "100%";
+export const GUTTER_PX = 30;
+export const GUTTER = `${GUTTER_PX}px`;
+export const GUTTER_HALF = `${GUTTER_PX / 2}px`;
+export const _ONE_THIRD = `${GUTTER_PX / 3}px`;
+export const SPACING_SCALE_1 = "5px";
+export const SPACING_SCALE_2 = "10px";
+export const SPACING_SCALE_3 = "15px";
+export const SPACING_SCALE_4 = "20px";
+export const SPACING_SCALE_5 = "30px";
+export const SPACING_SCALE_6 = "60px";
+export const NTA_LIGHT = `"nta", Arial, sans-serif`;
+export const NTA_LIGHT_TABULAR = `ntatabularnumbers", ${NTA_LIGHT}`;
+export const FONT_STACK = `${NTA_LIGHT} !default`;
+export const FONT_STACK_TABULAR = `${NTA_LIGHT_TABULAR} !default`;
+export const FONT_STACK_PRINT = "sans-serif !default";
