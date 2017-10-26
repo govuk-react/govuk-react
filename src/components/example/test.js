e68489872782eb4d20c7f3c82ad0fd9827f49efe
Alasdair McLeay <alasdair.mcleay@me.com>
2017-10-26 23:50:30 +0100

    remove example component

--- src/components/example/test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Component from './';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Component>Example</Component>, div);
-});
