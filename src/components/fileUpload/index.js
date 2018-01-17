d48b9e6824dc5f8b4dc09938f08e0dbb87ba14a1
Mark Chambers <me@marksy.com>
2018-01-17 17:04:42 +0000

    WIP: DRYing out some of the styling

--- src/components/fileUpload/index.js
+++ src/components/fileUpload/index.js
@@ -5,104 +5,21 @@ import React from "react";
 import glamorous from "glamorous";
 import PropTypes from "prop-types";
 
-import * as COLOUR from "govuk-colours";
-import { BREAKPOINTS, SITE_WIDTH } from "../../constants/index";
+import { STYLE_DEFAULT } from "../../constants/index";
 
-const mediaQueries = {
-  largeScreen: `@media only screen and (min-width: ${BREAKPOINTS.LARGESCREEN})`
-};
-
-const Label = glamorous.label(
-  {
-    marginBottom: "15px",
-    display: "flex",
-    flexDirection: "column",
-    [mediaQueries.largeScreen]: {
-      maxWidth: SITE_WIDTH,
-      marginBottom: "30px"
-    }
-  },
-  ({ errorText }) => ({
-    borderLeft: errorText ? `4px solid ${COLOUR.ERROR_COLOUR}` : "",
-    marginRight: errorText ? `15px` : "0",
-    paddingLeft: errorText ? `10px` : "0"
-  })
+const Label = glamorous.label(STYLE_DEFAULT.LABEL, ({ errorText }) =>
+  STYLE_DEFAULT.LABEL.ERROR(errorText)
 );
 
-const LabelText = glamorous.span(
-  {
-    fontFamily: '"nta", Arial, sans-serif',
-    WebkitFontSmoothing: "antialiased",
-    MozOsxFontSmoothing: "grayscale",
-    fontWeight: 400,
-    fontSize: "16px",
-    lineHeight: "1.25",
-    color: `${COLOUR.BLACK}`,
-    display: "block",
-    paddingBottom: "2px",
-    [mediaQueries.largeScreen]: {
-      fontSize: "19px",
-      lineHeight: "1.31579"
-    }
-  },
-  ({ errorText }) => ({
-    fontWeight: errorText ? 700 : LabelText.fontWeight
-  })
+const LabelText = glamorous.span(STYLE_DEFAULT.LABEL_TEXT, ({ errorText }) =>
+  STYLE_DEFAULT.LABEL_TEXT.ERROR(errorText)
 );
 
-const HintText = glamorous.span({
-  fontFamily: '"nta", Arial, sans-serif',
-  WebkitFontSmoothing: "antialiased",
-  MozOsxFontSmoothing: "grayscale",
-  fontWeight: 400,
-  textTransform: "none",
-  fontSize: "16px",
-  lineHeight: "1.25",
-  color: `${COLOUR.GREY_1}`,
-  [mediaQueries.largeScreen]: {
-    fontSize: "19px",
-    lineHeight: "1.31579"
-  }
-});
+const HintText = glamorous.span(STYLE_DEFAULT.HINT_TEXT);
 
-const ErrorText = glamorous.span({
-  fontFamily: '"nta", Arial, sans-serif',
-  WebkitFontSmoothing: "antialiased",
-  MozOsxFontSmoothing: "grayscale",
-  fontWeight: 700,
-  textTransform: "none",
-  fontSize: "16px",
-  lineHeight: "1.25",
-  color: `${COLOUR.ERROR_COLOUR}`,
-  paddingTop: "4px",
-  paddingBottom: "2px",
-  [mediaQueries.largeScreen]: {
-    fontSize: "19px",
-    lineHeight: "1.31579"
-  }
-});
+const ErrorText = glamorous.span(STYLE_DEFAULT.ERROR_TEXT);
 
-const Input = glamorous.input({
-  boxSizing: "border-box",
-  fontFamily: '"nta", Arial, sans-serif',
-  fontWeight: 400,
-  textTransform: "none",
-  fontSize: "small",
-  lineHeight: "inherit",
-  width: "100%",
-  padding: "5px 4px 4px",
-  border: `0`,
-  [mediaQueries.largeScreen]: {
-    width: "50%"
-  },
-  "[disabled]": {
-    cursor: "auto"
-  },
-  ":focus": {
-    outline: `3px solid ${COLOUR.YELLOW}`,
-    outlineOffset: 0
-  }
-});
+const Input = glamorous.input(STYLE_DEFAULT.INPUT);
 
 const FileUpload = ({ children, ...props }) => (
   <Label {...props}>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import { BLACK, GREY_1, YELLOW, ERROR_COLOUR } from "govuk-colours";
+
 export const BORDER_WIDTH_MOBILE = "4px";
 export const BORDER_WIDTH_TABLET = "5px";
 export const BORDER_WIDTH_FORM_ELEMENT = "2px";
@@ -11,18 +13,123 @@ export const BREAKPOINTS = {
   SMALLSCREEN: "320px",
   LARGESCREEN: "641px"
 };
+
+export const MEDIA_QUERIES = {
+  LARGESCREEN: `@media only screen and (min-width: ${BREAKPOINTS.LARGESCREEN})`
+};
+
 export const GUTTER_PX = 30;
 export const GUTTER = `${GUTTER_PX}px`;
 export const GUTTER_HALF = `${GUTTER_PX / 2}px`;
 export const GUTTER_ONE_THIRD = `${GUTTER_PX / 3}px`;
-export const SPACING_SCALE_1 = "5px";
-export const SPACING_SCALE_2 = "10px";
-export const SPACING_SCALE_3 = "15px";
-export const SPACING_SCALE_4 = "20px";
-export const SPACING_SCALE_5 = "30px";
-export const SPACING_SCALE_6 = "60px";
+export const SPACING = {
+  SCALE_1: "5px",
+  SCALE_2: "10px",
+  SCALE_3: "15px",
+  SCALE_4: "20px",
+  SCALE_5: "30px",
+  SCALE_6: "60px"
+};
 export const NTA_LIGHT = `"nta", Arial, sans-serif`;
 export const NTA_LIGHT_TABULAR = `ntatabularnumbers", ${NTA_LIGHT}`;
 export const FONT_STACK = `${NTA_LIGHT} !default`;
 export const FONT_STACK_TABULAR = `${NTA_LIGHT_TABULAR} !default`;
 export const FONT_STACK_PRINT = "sans-serif !default";
+export const FONT_SIZE = {
+  SIZE_14: "14px",
+  SIZE_16: "16px",
+  SIZE_19: "19px"
+};
+
+export const STYLE_DEFAULT = {
+  LABEL: {
+    marginBottom: SPACING.SCALE_3,
+    display: "flex",
+    flexDirection: "column",
+    boxSizing: "border-box",
+    [MEDIA_QUERIES.LARGESCREEN]: {
+      maxWidth: SITE_WIDTH,
+      marginBottom: SPACING.SCALE_5
+    },
+    ERROR(errorText) {
+      return {
+        borderLeft: errorText
+          ? `${BORDER_WIDTH_ERROR} solid ${ERROR_COLOUR}`
+          : "",
+        marginRight: errorText ? `${SPACING.SCALE_3}` : "0",
+        paddingLeft: errorText ? `${SPACING.SCALE_2}` : "0"
+      };
+    }
+  },
+  LABEL_TEXT: {
+    fontFamily: NTA_LIGHT,
+    WebkitFontSmoothing: "antialiased",
+    MozOsxFontSmoothing: "grayscale",
+    fontWeight: 400,
+    fontSize: FONT_SIZE.SIZE_16,
+    lineHeight: "1.25",
+    color: `${BLACK}`,
+    display: "block",
+    paddingBottom: "2px",
+    [MEDIA_QUERIES.LARGESCREEN]: {
+      fontSize: FONT_SIZE.SIZE_19,
+      lineHeight: "1.31579"
+    },
+    ERROR(errorText) {
+      return {
+        fontWeight: errorText ? 700 : STYLE_DEFAULT.LABEL_TEXT.fontWeight
+      };
+    }
+  },
+  HINT_TEXT: {
+    fontFamily: NTA_LIGHT,
+    WebkitFontSmoothing: "antialiased",
+    MozOsxFontSmoothing: "grayscale",
+    fontWeight: 400,
+    textTransform: "none",
+    fontSize: FONT_SIZE.SIZE_16,
+    lineHeight: "1.25",
+    color: `${GREY_1}`,
+    [MEDIA_QUERIES.LARGESCREEN]: {
+      fontSize: FONT_SIZE.SIZE_19,
+      lineHeight: "1.31579"
+    }
+  },
+  ERROR_TEXT: {
+    fontFamily: NTA_LIGHT,
+    WebkitFontSmoothing: "antialiased",
+    MozOsxFontSmoothing: "grayscale",
+    fontWeight: 700,
+    textTransform: "none",
+    fontSize: FONT_SIZE.SIZE_16,
+    lineHeight: "1.25",
+    color: `${ERROR_COLOUR}`,
+    paddingTop: "4px",
+    paddingBottom: "2px",
+    [MEDIA_QUERIES.LARGESCREEN]: {
+      fontSize: FONT_SIZE.SIZE_19,
+      lineHeight: "1.31579"
+    }
+  },
+  INPUT: {
+    boxSizing: "border-box",
+    fontFamily: NTA_LIGHT,
+    fontWeight: 400,
+    textTransform: "none",
+    fontSize: "small",
+    lineHeight: "inherit",
+    width: "100%",
+    padding: "5px 4px 4px",
+    border: `0`,
+    [MEDIA_QUERIES.LARGESCREEN]: {
+      width: "50%"
+    },
+    "[disabled]": {
+      cursor: "auto"
+    },
+    ":focus": {
+      outline: `3px solid ${YELLOW}`,
+      outlineOffset: 0
+    }
+  }
+};
