c761f501b9b20f109555c4de90f93901d6a49228
Mark Chambers <me@marksy.com>
2018-01-17 12:26:01 +0000

    fixed eslint warning

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,12 +5,12 @@ import "normalize.css";
 
 import "./styles.css";
 
-import '../components/breadcrumb/stories'
-import '../components/inputField/stories'
-import '../components/textarea/stories'
-import '../components/button/stories'
-import '../components/radio/stories'
-import '../components/checkbox/stories'
-import '../components/layout/stories'
-import '../components/select/stories'
-import '../components/fileUpload/stories'
+import "../components/breadcrumb/stories";
+import "../components/inputField/stories";
+import "../components/textarea/stories";
+import "../components/button/stories";
+import "../components/radio/stories";
+import "../components/checkbox/stories";
+import "../components/layout/stories";
+import "../components/select/stories";
+import "../components/fileUpload/stories";
