8c0c4f15143284ae3cb1d30c5ef6d9a15fb6105c
Alasdair McLeay <alasdair.mcleay@me.com>
2018-01-22 18:04:20 +0000

    export all components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+export { default as BackLink } from "./components/BackLink";
 export { default as Breadcrumb } from "./components/Breadcrumb";
 export { default as Button } from "./components/Button";
 export { default as Checkbox } from "./components/Checkbox";
+export { default as DateInput } from "./components/DateInput";
+export { default as ErrorText } from "./components/ErrorText";
+export { default as FileUpload } from "./components/FileUpload";
+export { default as HintText } from "./components/HintText";
+export { default as Input } from "./components/Input";
+export { default as InputField } from "./components/InputField";
+export { default as Label } from "./components/Label";
+export { default as LabelText } from "./components/LabelText";
+export { default as Layout } from "./components/Layout";
+export { default as Radio } from "./components/Radio";
+export { default as Select } from "./components/Select";
+export { default as TextArea } from "./components/TextArea";
