b8bb7384b9380261753c6d935c526ce314f5673b
Mark Chambers <me@marksy.com>
2018-01-23 15:05:33 +0000

    - added enzyme snapshot testing to panel component - added script to update snapshot

--- src/components/panel/test.js
+++ src/components/panel/test.js
@@ -1,35 +1,22 @@
 import React from "react";
-import { shallow, render, mount } from "enzyme";
-import { matcher, serializer } from "jest-glamor-react";
+import ReactDOM from "react-dom";
+import { shallow, mount, render } from "enzyme";
 
-import { Panel, PanelTitle, PanelBody } from "./";
-
-// This is what adds the CSS to the output snapshot
-expect.addSnapshotSerializer(serializer);
-
-// this adds toMatchSnapshot to expect and makes the snapshot diff output look nice in the terminal
-expect.extend(matcher);
+import Panel from "./";
 
 describe("Panel", () => {
+  const titleExample = "Example";
+  const bodyExample = "body";
+  const wrapper = <Panel panelTitle={titleExample} panelBody={bodyExample} />;
+
   it("renders without crashing", () => {
-    const titleExample = "Example";
-    const bodyExample = "body";
+    const div = document.createElement("div");
+    ReactDOM.render(wrapper, div);
+  });
 
-    const ui = (
-      <Panel panelTitle={titleExample}>
-        <PanelTitle>{titleExample}</PanelTitle>
-        <PanelBody>{bodyExample}</PanelBody>
-      </Panel>
-    );
-
-    expect(shallow(ui).text()).toContain(titleExample);
-    expect(mount(ui).text()).toContain(titleExample);
-    expect(render(ui).text()).toContain(titleExample);
-    // const wrapper = shallow(
-    //   <Panel panelTitle={titleExample} panelBody={bodyExample} />
-    // );
-    // expect(wrapper.text()).toContain(titleExample);
-    // expect(wrapper.text()).not.toContain("Upset lobster");
-    // expect(wrapper.text()).toContain(bodyExample);
+  it("matches snapshot", () => {
+    expect(shallow(wrapper)).toMatchSnapshot(`enzyme.shallow`);
+    expect(mount(wrapper)).toMatchSnapshot(`enzyme.mount`);
+    expect(render(wrapper)).toMatchSnapshot(`enzyme.render`);
   });
 });
