86032e7016723091aea7a0bfba5a1be5b5b2ccbe
Mark Chambers <me@marksy.com>
2018-01-23 15:40:21 +0000

    added TODO comment for backlink, moved jest-glamor-react intodevDep

--- src/components/backLink/test.js
+++ src/components/backLink/test.js
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom";
 import BackLink from "./";
 
 describe("Back Link", () => {
+  // TODO use sinon to confirm that the click handler is called when the button is clicked
   const testFn = () => false;
   it("renders without crashing", () => {
     const div = document.createElement("div");
