3e3c198c2113ed8b8a5ecbe6bb954069fb196bc8
Mark Chambers <me@marksy.com>
2018-01-23 09:17:19 +0000

    wip trying to get jest enzyme glam to run

--- src/components/panel/test.js
+++ src/components/panel/test.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { shallow, render, mount } from "enzyme";
+import { matcher, serializer } from "jest-glamor-react";
+
 import { Panel, PanelTitle, PanelBody } from "./";
+
+// This is what adds the CSS to the output snapshot
+expect.addSnapshotSerializer(serializer);
+
+// this adds toMatchSnapshot to expect and makes the snapshot diff output look nice in the terminal
+expect.extend(matcher);
 
 describe("Panel", () => {
   it("renders without crashing", () => {
