75003c00c19309d39f340745ebaa68ffd005075e
Alasdair McLeay <alasdair.mcleay@me.com>
2018-01-24 22:54:38 +0000

    eslint

--- components/govuk-react-example/src/App.js
+++ components/govuk-react-example/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Form, Field } from 'react-final-form'
+import React from 'react';
+import { Form, Field } from 'react-final-form';
 import { Button, DateInput } from 'govuk-react';
 
 // based on https://codesandbox.io/s/40mr0v2r87
@@ -10,44 +10,47 @@ const DateInputAdapter = ({ input, meta, ...rest }) => (
     onChange={(event, value) => input.onChange(value)}
     errorText={meta.touched ? meta.error : ''}
   />
-)
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+);
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const onSubmit = async values => {
-  await sleep(300)
-  window.alert(JSON.stringify(values, 0, 2))
-}
-const required = value => (value ? undefined : 'Required')
+const onSubmit = async (values) => {
+  await sleep(300);
+  window.alert(JSON.stringify(values, 0, 2));
+};
+const required = value => (value ? undefined : 'Required');
 
 const App = () => (
   <div>
     <Form
       onSubmit={onSubmit}
-      render={({ handleSubmit, reset, submitting, pristine, values }) => (
-        <form onSubmit={handleSubmit}>
-          <div>
-            <Field
-              name="dob"
-              component={DateInputAdapter}
-              validate={required}
-              hintText="Date of birth"
-              labelPosition="right"
-            />
-          </div>
-          <Button type="submit" disabled={submitting}>
+      render={({
+ handleSubmit, reset, submitting, pristine, values,
+}) => (
+  <form onSubmit={handleSubmit}>
+    <div>
+      <Field
+        name="dob"
+        component={DateInputAdapter}
+        validate={required}
+        hintText="Date of birth"
+        labelPosition="right"
+      />
+    </div>
+    <Button type="submit" disabled={submitting}>
             Submit
-          </Button>
-          <Button
-            type="button"
-            onClick={reset}
-            disabled={submitting || pristine}>
+    </Button>
+    <Button
+      type="button"
+      onClick={reset}
+      disabled={submitting || pristine}
+    >
             Reset
-          </Button>
-          <pre>{JSON.stringify(values, 0, 2)}</pre>
-        </form>
+    </Button>
+    <pre>{JSON.stringify(values, 0, 2)}</pre>
+  </form>
       )}
     />
   </div>
-)
+);
 
 export default App;
