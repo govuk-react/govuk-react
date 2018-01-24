abed64d603bdb2d0cca5a6964c829f70606b1582
Alasdair McLeay <alasdair.mcleay@me.com>
2018-01-24 22:41:56 +0000

    use react-final-form

--- components/govuk-react-example/README.md
+++ components/govuk-react-example/README.md
@@ -1,6 +1,6 @@
 # govuk-react-example
 
-Example usage of [govuk-react](https://github.com/penx/govuk-react) using [create-react-app](https://github.com/facebookincubator/create-react-app/).
+Example usage of [govuk-react](https://github.com/penx/govuk-react) using [create-react-app](https://github.com/facebookincubator/create-react-app/) and [react-final-form](https://github.com/final-form/react-final-form).
 
 To run this project locally:
 
--- components/govuk-react-example/src/App.js
+++ components/govuk-react-example/src/App.js
@@ -1,29 +1,53 @@
-import React, { Component } from 'react';
-import { Breadcrumb, Button, Checkbox, DateInput } from 'govuk-react';
+import React from 'react'
+import { Form, Field } from 'react-final-form'
+import { Button, DateInput } from 'govuk-react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Breadcrumb>
-          <a href="/section">Section 1</a>
-          <a href="/section/sub-section">Sub-section</a>
-          <a href="/section/three">Three sections and a long line</a>
-        </Breadcrumb>
-        <Checkbox>Waste from animal carcasses</Checkbox>
-        <Checkbox>Waste from mines or quarries</Checkbox>
-        <Checkbox>Farm or agricultural waste</Checkbox>
-        <DateInput
-          hintText="For example, 31 03 1980"
-          errorText="Error message goes here"
-        >
-          What is your date of birth?
-        </DateInput>
+// based on https://codesandbox.io/s/40mr0v2r87
+const DateInputAdapter = ({ input, meta, ...rest }) => (
+  <DateInput
+    {...input}
+    {...rest}
+    onChange={(event, value) => input.onChange(value)}
+    errorText={meta.touched ? meta.error : ''}
+  />
+)
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-        <Button start>Start now</Button>
-      </div>
-    );
-  }
+const onSubmit = async values => {
+  await sleep(300)
+  window.alert(JSON.stringify(values, 0, 2))
 }
+const required = value => (value ? undefined : 'Required')
+
+const App = () => (
+  <div>
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit, reset, submitting, pristine, values }) => (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <Field
+              name="dob"
+              component={DateInputAdapter}
+              validate={required}
+              hintText="Date of birth"
+              labelPosition="right"
+            />
+          </div>
+          <Button type="submit" disabled={submitting}>
+            Submit
+          </Button>
+          <Button
+            type="button"
+            onClick={reset}
+            disabled={submitting || pristine}>
+            Reset
+          </Button>
+          <pre>{JSON.stringify(values, 0, 2)}</pre>
+        </form>
+      )}
+    />
+  </div>
+)
 
 export default App;
