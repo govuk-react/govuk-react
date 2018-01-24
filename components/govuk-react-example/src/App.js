63349066ceaf32f6e87385530ca2f0d2c49db183
Alasdair McLeay <alasdair.mcleay@me.com>
2018-01-24 21:36:54 +0000

    basic usage of govuk-react

--- components/govuk-react-example/src/App.js
+++ components/govuk-react-example/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Breadcrumb, Button, Checkbox, DateInput } from 'govuk-react';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Breadcrumb>
+          <a href="/section">Section 1</a>
+          <a href="/section/sub-section">Sub-section</a>
+          <a href="/section/three">Three sections and a long line</a>
+        </Breadcrumb>
+        <Checkbox>Waste from animal carcasses</Checkbox>
+        <Checkbox>Waste from mines or quarries</Checkbox>
+        <Checkbox>Farm or agricultural waste</Checkbox>
+        <DateInput
+          hintText="For example, 31 03 1980"
+          errorText="Error message goes here"
+        >
+          What is your date of birth?
+        </DateInput>
+
+        <Button start>Start now</Button>
       </div>
     );
   }
