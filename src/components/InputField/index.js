3d46cce0644169aab940c956801118c6e2392184
Mark Chambers <me@marksy.com>
2018-01-26 17:02:41 +0000

    trying to expose the value of the component to the parent

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -10,28 +10,61 @@ import ErrorText from "../ErrorText/index";
 import HintText from "../HintText/index";
 import Input from "../Input/index";
 
-const InputField = ({ children, errorText, hintText }) => (
-  <Label errorText={errorText}>
-    <LabelText errorText={errorText}>{children}</LabelText>
-    {hintText ? <HintText>{hintText}</HintText> : <span />}
-    {errorText ? (
-      <ErrorText errorText={errorText}>{errorText}</ErrorText>
-    ) : (
-      <span />
-    )}
-    <Input type="text" errorText={errorText} />
-  </Label>
-);
+// const InputField = ({ children, errorText, hintText, onChange }) => (
+
+class InputField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { value: "" };
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+    // console.log(this.state);
+  }
+
+  render() {
+    const value = this.state.value;
+    return (
+      <Label errorText={this.props.errorText}>
+        <LabelText errorText={this.props.errorText}>
+          {this.props.children}
+        </LabelText>
+        {this.props.hintText ? (
+          <HintText>{this.props.hintText}</HintText>
+        ) : (
+          <span />
+        )}
+        {this.props.errorText ? (
+          <ErrorText errorText={this.props.errorText}>
+            {this.props.errorText}
+          </ErrorText>
+        ) : (
+          <span />
+        )}
+        <Input
+          type="text"
+          value={this.state.value}
+          errorText={this.props.errorText}
+          onChange={this.props.onChange}
+        />
+      </Label>
+    );
+  }
+}
 
 InputField.defaultProps = {
   hintText: null,
-  errorText: null
+  errorText: null,
+  onChange: undefined
 };
 
 InputField.propTypes = {
   children: PropTypes.node.isRequired,
   hintText: PropTypes.string,
-  errorText: PropTypes.string
+  errorText: PropTypes.string,
+  onChange: PropTypes.func
 };
 
 export default InputField;
