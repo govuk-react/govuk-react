913effeaf6dfb6ed868df7176eb4053973b0c968
Alasdair McLeay <alasdair.mcleay@me.com>
2018-01-29 15:37:39 +0000

    remove react-final-form-prop-types, use meta data from final form for error message

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -3,7 +3,6 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import { FieldRenderProps } from "react-final-form-prop-types";
 
 import Label from "../Label/index";
 import LabelText from "../LabelText/index";
@@ -12,32 +11,44 @@ import HintText from "../HintText/index";
 import Input from "../Input/index";
 
 const InputField = props => (
-  <Label errorText={props.errorText}>
-    <LabelText errorText={props.errorText}>{props.children}</LabelText>
-    {props.hintText ? <HintText>{props.hintText}</HintText> : <span />}
-    {props.errorText ? (
-      <ErrorText errorText={props.errorText}>{props.errorText}</ErrorText>
-    ) : (
-      <span />
-    )}
+  <Label error={props.meta.touched && props.meta.error}>
+    <LabelText>{props.children}</LabelText>
+    {props.hint && <HintText>{props.hint}</HintText>}
+    {props.meta.touched &&
+      props.meta.error && <ErrorText>{props.meta.error}</ErrorText>}
     <Input type="text" {...props.input} />
   </Label>
 );
 
 InputField.defaultProps = {
-  hintText: null,
-  errorText: null,
-  onChange: null,
-  value: undefined
+  hint: null
 };
 
 InputField.propTypes = {
-  ...FieldRenderProps,
-  children: PropTypes.node.isRequired,
-  hintText: PropTypes.string,
-  errorText: PropTypes.string,
-  value: PropTypes.string,
-  onChange: PropTypes.func
+  hint: PropTypes.string,
+  input: PropTypes.shape({
+    name: PropTypes.string,
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
+    onFocus: PropTypes.func,
+    value: PropTypes.any
+  }).isRequired,
+  meta: PropTypes.shape({
+    active: PropTypes.bool,
+    dirty: PropTypes.bool,
+    dirtySinceLastSubmit: PropTypes.bool,
+    error: PropTypes.any,
+    initial: PropTypes.bool,
+    invalid: PropTypes.bool,
+    pristine: PropTypes.bool,
+    submitError: PropTypes.any,
+    submitFailed: PropTypes.bool,
+    submitSucceeded: PropTypes.bool,
+    touched: PropTypes.bool,
+    valid: PropTypes.bool,
+    visited: PropTypes.bool
+  }).isRequired,
+  children: PropTypes.node.isRequired
 };
 
 export default InputField;
