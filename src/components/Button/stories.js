cfb9923a9a08163dc5224c0834875ef0e23d220d
Mark Chambers <marksy@users.noreply.github.com>
2018-02-05 15:31:08 +0000

    - added storybook knobs (#54)
    - added knob example to buttons

--- src/components/Button/stories.js
+++ src/components/Button/stories.js
@@ -1,26 +1,36 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withKnobs, text, boolean } from "@storybook/addon-knobs/react";
+
 import iconPointer from "../../../images/icon-pointer.png";
 import Button from ".";
 
-storiesOf("Button", module).add("Button text", () => (
-  <Button>Save and continue</Button>
+const stories = storiesOf("Button", module);
+
+stories.addDecorator(withKnobs);
+
+stories.add("Button text", () => <Button>Save and continue</Button>);
+
+stories.add("Start", () => (
+  <Button start={boolean("Start", true)}>Start now</Button>
 ));
-storiesOf("Button", module).add("Start", () => (
-  <Button start>Start now</Button>
-));
-storiesOf("Button", module).add("Start with icon", () => (
-  <Button iconUrl={iconPointer} start>
+
+stories.add("Start with icon", () => (
+  <Button iconUrl={text("Icon", iconPointer)} start={boolean("Start", true)}>
     Start now
   </Button>
 ));
-storiesOf("Button", module).add("Disabled", () => (
-  <Button disabled aria-disabled="true">
-    Disabled primary button
-  </Button>
+
+stories.add("Disabled", () => (
+  <Button disabled={boolean("Disabled", true)}>Disabled primary button</Button>
 ));
-storiesOf("Button", module).add("Disabled start with icon", () => (
-  <Button disabled iconUrl={iconPointer} start aria-disabled="true">
+
+stories.add("Disabled start with icon", () => (
+  <Button
+    disabled={boolean("Disabled", true)}
+    iconUrl={text("Icon", iconPointer)}
+    start={boolean("Start", true)}
+  >
     Start now
   </Button>
 ));
