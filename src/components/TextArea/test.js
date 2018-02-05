0849a50fc78165cbe9ccb28dd7255a2d0accf9e6
Alasdair McLeay <penx@users.noreply.github.com>
2018-02-05 17:01:31 +0000

    only use enzyme.mount for snapshots (#55)
    to reduce noise in PRs

--- src/components/TextArea/test.js
+++ src/components/TextArea/test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { shallow, mount, render } from "enzyme";
+import { mount } from "enzyme";
 import Textarea from "./";
 
 describe("Textarea", () => {
@@ -13,8 +13,6 @@ describe("Textarea", () => {
   });
 
   it("matches snapshot", () => {
-    expect(shallow(wrapper)).toMatchSnapshot(`enzyme.shallow`);
     expect(mount(wrapper)).toMatchSnapshot(`enzyme.mount`);
-    expect(render(wrapper)).toMatchSnapshot(`enzyme.render`);
   });
 });
