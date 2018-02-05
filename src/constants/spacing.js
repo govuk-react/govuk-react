293646af8fe0fb282f15573eefb80cf4e3e176d3
Alasdair McLeay <penx@users.noreply.github.com>
2018-02-05 10:20:03 +0000

    Feature/headers (#41)
    * typography constants
    
    * print media query constant
    
    * Header component
    
    * alphabetically sort story imports and add Header
    
    * bugfix: storybook action on BackLink
    
    * Basic header tests
    
    * remove old todo
    
    * added margin 0 to headings so they behave as atoms
    
    * updated header snapshots after margin change
    
    * export headers from root index, alphabetically sort
    
    * Make headers have margins by default, maybe add prop to disable margins later
    
    * updated Header snapshots with css post rebase
    
    * update header constants to be mobile first

---
+++ src/constants/spacing.js
@@ -0,0 +1,51 @@
+// https://github.com/alphagov/govuk-frontend/blob/master/src/globals/scss/settings/_spacing.scss
+
+export const RESPONSIVE_0 = {
+  mobile: 0,
+  tablet: 0
+};
+
+export const RESPONSIVE_1 = {
+  mobile: 5,
+  tablet: 5
+};
+
+export const RESPONSIVE_2 = {
+  mobile: 10,
+  tablet: 10
+};
+
+export const RESPONSIVE_3 = {
+  mobile: 15,
+  tablet: 15
+};
+
+export const RESPONSIVE_4 = {
+  mobile: 15,
+  tablet: 20
+};
+
+export const RESPONSIVE_5 = {
+  mobile: 15,
+  tablet: 25
+};
+
+export const RESPONSIVE_6 = {
+  mobile: 20,
+  tablet: 30
+};
+
+export const RESPONSIVE_7 = {
+  mobile: 25,
+  tablet: 40
+};
+
+export const RESPONSIVE_8 = {
+  mobile: 30,
+  tablet: 50
+};
+
+export const RESPONSIVE_9 = {
+  mobile: 40,
+  tablet: 60
+};
