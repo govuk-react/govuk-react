b50ec20a847d784c070ce1bd5e7d0fad216f7f4f
Mark Chambers <mark.chambers1@ibm.com@marks-air.sbank.uk.ibm.com>
2018-02-08 14:43:48 +0000

    removed comments

--- src/stories/components/test.js
+++ src/stories/components/test.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import FinalFormWrapper from "./FinalFormWrapper";
-// import * as FinalFormWrapper from 'somemodule';
 
 describe("FinalFormWrapper", () => {
   const example = "example";
