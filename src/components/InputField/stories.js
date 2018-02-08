77eae22affcab9980a700e5f32846e57e9a33758
Mark Chambers <mark.chambers1@ibm.com@marks-air.sbank.uk.ibm.com>
2018-02-08 16:12:50 +0000

    fixed components that didnt have updated props to suit Final Form

--- src/components/InputField/stories.js
+++ src/components/InputField/stories.js
@@ -2,6 +2,11 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import InputField from ".";
 
+const meta = {
+  touched: true,
+  error: "Example"
+};
+
 storiesOf("InputField", module).add("Component default", () => (
   <InputField name="group0">National Insurance number</InputField>
 ));
@@ -27,28 +32,8 @@ storiesOf("InputField", module).add("Input with HintText & error", () => (
       <br />,
       "For example, ‘QQ 12 34 56 C’."
     ]}
-    errorText="Incorrect format"
+    meta={meta}
   >
     National Insurance number
   </InputField>
-));
-
-storiesOf("InputField", module).add("Multiple Inputs", () => (
-  <div>
-    <InputField name="group1">Your name</InputField>
-    <InputField name="group1">Your quest</InputField>
-    <InputField name="group1" errorText="Blue. No, yellow!">
-      Your favourite colour
-    </InputField>
-    <InputField
-      name="group1"
-      hint={[
-        "It’s on your National Insurance card, benefit letter, payslip or P60.",
-        <br />,
-        "For example, ‘QQ 12 34 56 C’."
-      ]}
-    >
-      National Insurance number
-    </InputField>
-  </div>
 ));
--- src/components/TextArea/stories.js
+++ src/components/TextArea/stories.js
@@ -3,12 +3,17 @@ import { storiesOf } from "@storybook/react";
 
 import TextArea from ".";
 
+const meta = {
+  touched: true,
+  error: "Example"
+};
+
 storiesOf("Textarea", module).add("Textarea", () => (
   <TextArea name="group1">Description of what you saw</TextArea>
 ));
 
 storiesOf("Textarea", module).add("Textarea with HintText", () => (
-  <TextArea name="group1" hintText={["Enter as many words as you like"]}>
+  <TextArea name="group1" hint={["Enter as many words as you like"]}>
     Description of what you saw
   </TextArea>
 ));
@@ -16,8 +21,8 @@ storiesOf("Textarea", module).add("Textarea with HintText", () => (
 storiesOf("Textarea", module).add("Textarea with HintText & error", () => (
   <TextArea
     name="group1"
-    hintText={["Enter as many words as you like"]}
-    errorText="Please enter a description"
+    hint={["Enter as many words as you like"]}
+    meta={meta}
   >
     Description of what you saw
   </TextArea>
