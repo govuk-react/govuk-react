1709f6e6b09f51ce5e8d343e0e0308dae420d58d
Mark Chambers <mark.chambers1@ibm.com@marks-air.sbank.uk.ibm.com>
2018-02-12 17:02:55 +0000

    added beta banner

---
+++ src/components/BetaBanner/stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import betaBanner from ".";
+
+storiesOf("betaBanner", module).add("betaBanner", () => (
+  <betaBanner>betaBanner example</betaBanner>
+));
