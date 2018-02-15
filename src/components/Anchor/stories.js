dfecb1ba655d99f7f70ecce79e0c9cca606b4857
Mark Chambers <mark.chambers1@ibm.com@marks-air.sbank.uk.ibm.com>
2018-02-15 11:48:41 +0000

    added wip

--- src/components/Anchor/stories.js
+++ src/components/Anchor/stories.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-
+// import { Link } from "react-router";
 import Anchor from ".";
 
+const ReactLink = <div>stuff</div>;
+
 storiesOf("Anchor", module).add("Anchor default", () => (
-  <Anchor hyperLink="https://example.com">Anchor example</Anchor>
+  <Anchor hyperLink="https://example.com" label="label" target="_blank">
+    Anchor example
+  </Anchor>
+));
+
+storiesOf("Anchor", module).add("Link example", () => (
+  <Anchor hyperLink="https://example.com" component={ReactLink}>
+    link example
+  </Anchor>
 ));
