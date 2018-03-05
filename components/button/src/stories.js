d6a91a144a56c1a0288fda8c88d8ed78f0669492
Alasdair McLeay <alasdair.mcleay@me.com>
2018-03-05 11:28:41 +0000

    disable import of images for now, should be using icons

--- components/button/src/stories.js
+++ components/button/src/stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs/react';
 
-import iconPointer from '../../../images/icon-pointer.png';
+// import iconPointer from '../../../images/icon-pointer.png';
 import Button from '.';
 
 const stories = storiesOf('Button', module);
@@ -15,22 +15,24 @@ stories.add('Start', () => (
   <Button start={boolean('Start', true)}>Start now</Button>
 ));
 
-stories.add('Start with icon', () => (
-  <Button iconUrl={text('Icon', iconPointer)} start={boolean('Start', true)}>
-    Start now
-  </Button>
-));
+// TODO: reinstate inline images
+// stories.add('Start with icon', () => (
+//   <Button iconUrl={text('Icon', iconPointer)} start={boolean('Start', true)}>
+//     Start now
+//   </Button>
+// ));
 
 stories.add('Disabled', () => (
   <Button disabled={boolean('Disabled', true)}>Disabled primary button</Button>
 ));
 
-stories.add('Disabled start with icon', () => (
-  <Button
-    disabled={boolean('Disabled', true)}
-    iconUrl={text('Icon', iconPointer)}
-    start={boolean('Start', true)}
-  >
-    Start now
-  </Button>
-));
+// TODO: reinstate inline images
+// stories.add('Disabled start with icon', () => (
+//   <Button
+//     disabled={boolean('Disabled', true)}
+//     iconUrl={text('Icon', iconPointer)}
+//     start={boolean('Start', true)}
+//   >
+//     Start now
+//   </Button>
+// ));
