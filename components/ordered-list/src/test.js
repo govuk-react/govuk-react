de1e86a1109fb7b9c023e6ba4781be97515de9e0
Mark Chambers <markips@gmail.com>
2018-03-07 14:42:17 +0000

    added tests

--- components/ordered-list/src/test.js
+++ components/ordered-list/src/test.js
@@ -6,5 +6,9 @@ describe(OrderedList, () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<OrderedList>Example</OrderedList>, div);
+    ReactDOM.render(
+      <OrderedList listStyleType="lower-alpha">Example</OrderedList>,
+      div,
+    );
   });
 });
