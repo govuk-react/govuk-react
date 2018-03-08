80a5cbd852bbd796639545336a9514984afbe7b5
Mark Chambers <markips@gmail.com>
2018-03-08 10:52:25 +0000

    added font styling to UL/OL

--- components/unordered-list/src/index.js
+++ components/unordered-list/src/index.js
@@ -3,13 +3,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import glamorous from 'glamorous';
+import {
+  FONT_SIZE,
+  LINE_HEIGHT,
+  MEDIA_QUERIES,
+  NTA_LIGHT,
+} from '@govuk-react/constants';
 
 const GUnorderedList = glamorous.ul(
   {
     margin: 0,
     padding: 0,
     paddingLeft: '20px',
+    fontFamily: NTA_LIGHT,
+    fontWeight: 400,
+    textTransform: 'none',
+    fontSize: FONT_SIZE.SIZE_14,
+    lineHeight: LINE_HEIGHT.SIZE_14,
     width: '100%',
+    [MEDIA_QUERIES.LARGESCREEN]: {
+      fontSize: FONT_SIZE.SIZE_16,
+      lineHeight: LINE_HEIGHT.SIZE_16,
+    },
   },
   ({ listStyleType }) => ({
     listStyleType: listStyleType || undefined,
