203ad7a7f34c9af6c7335a0f51f3322d4c85a01f
Mark Chambers <marksy@users.noreply.github.com>
2018-03-12 12:49:40 +0000

    Fix for duff breadcrumb children (#127)
    * added fixes
    
    * fix for breadcrumb issue with duff children resolves #123
    
    * added unit test to test if empty node is rendered
    
    * added TODOs to the code to remind us to update at later date

--- components/breadcrumb/src/test.js
+++ components/breadcrumb/src/test.js
@@ -6,10 +6,18 @@ import Breadcrumb from './';
 
 describe('breadcrumb', () => {
   const example = 'example';
+  const emptyNode = [];
   const wrapper = <Breadcrumb>{example}</Breadcrumb>;
   const wrapperMultiple = (
     <Breadcrumb>
       <a href="/section">Section 1</a>
+      {example}
+    </Breadcrumb>
+  );
+  const wrapperEmptyNode = (
+    <Breadcrumb>
+      <a href="/section">Section 1</a>
+      {emptyNode}
       {example}
     </Breadcrumb>
   );
@@ -19,10 +27,17 @@ describe('breadcrumb', () => {
     ReactDOM.render(wrapper, div);
   });
 
-  it('should render an unordered list', () => {
+  it('should render an item in unordered list', () => {
     const output = mount(wrapper);
     expect(output.find('ul')).toHaveLength(1);
+  });
+
+  it('should render multiple items in unordered list', () => {
     expect(mount(wrapperMultiple).find('ul li')).toHaveLength(2);
+  });
+
+  it('should render an unordered list without ghost/duff children', () => {
+    expect(mount(wrapperEmptyNode).find('ul li')).toHaveLength(2);
   });
 
   it('matches snapshot', () => {
