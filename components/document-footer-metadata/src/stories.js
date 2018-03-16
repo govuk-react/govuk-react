62c77c2eeabae1bde5006a5f48dd05fa4938fcd5
Mark Chambers <marksy@users.noreply.github.com>
2018-03-16 14:55:04 +0000

    Feature/document footer meta data (#130)
    * wip
    
    * document footer metadata wip
    
    * styles wip
    
    * fixed issue where unordered list was being rendered without children
    
    * fixed issue where proptype was expecting object
    
    * updated componet example to reflect how the component is intended to be used

---
+++ components/document-footer-metadata/src/stories.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { asAnchor } from '@govuk-react/hoc';
+
+import DocumentFooterMetadata from '.';
+
+const AnchorTag = asAnchor('a');
+
+const fromData = [
+  <AnchorTag href="/government/organisations/ministry-of-defence">Ministry of Defence</AnchorTag>,
+];
+
+const partOfData = [
+  <AnchorTag href="/government/topics/energy">Energy</AnchorTag>,
+  <AnchorTag href="/government/topics/environment">Environment</AnchorTag>,
+];
+
+const otherData = [
+  {
+    id: 0,
+    title: 'Consultation type',
+    content: <AnchorTag href="/government/publications">Open</AnchorTag>,
+  },
+  {
+    id: 1,
+    title: 'Published',
+    content: '20 January 2012',
+  },
+  {
+    id: 2,
+    title: 'Date opened',
+    content: '1 February 2012',
+  },
+  {
+    id: 3,
+    title: 'Date closed',
+    content: '1 March 2013',
+  },
+];
+
+storiesOf('Document Footer Metadata', module).add('DFM From example', () => (
+  <DocumentFooterMetadata
+    from={fromData}
+  />
+));
+
+storiesOf('Document Footer Metadata', module).add('DFM From & part of example', () => (
+  <DocumentFooterMetadata
+    from={fromData}
+    partOf={partOfData}
+  />
+));
+
+storiesOf('Document Footer Metadata', module).add('DFM From & other data example', () => (
+  <DocumentFooterMetadata
+    from={fromData}
+    other={otherData}
+  />
+));
