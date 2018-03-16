3f85b4f527a4b3a8aa73dac2c70101bd5f66affc
Alasdair McLeay <penx@users.noreply.github.com>
2018-03-16 15:35:22 +0000

    Feature/api docs as package (#128)
    Move the API docs script in to their own package so it can be used as a dependency by other projects

--- packages/api-docs/src/generateComponentImage.js
+++ packages/api-docs/src/generateComponentImage.js
@@ -1,25 +1,25 @@
 /* eslint-disable no-console */
 
-import React from "react";
-import Jimp from "jimp";
-import chalk from "chalk";
+import React from 'react';
+import Jimp from 'jimp';
+import chalk from 'chalk';
 
-import generateImage from "./generateImage";
+import generateImage from './generateImage';
 
 export default async function generateComponentImage(
   file,
   Component,
-  imagePath
+  imagePath,
 ) {
   try {
     const image = await generateImage(React.createElement(Component), {
       image: {
-        omitBackground: false
-      }
+        omitBackground: false,
+      },
     });
     const jimage = await Jimp.read(image);
     jimage.autocrop(false).write(imagePath);
   } catch (e) {
-    console.log(chalk.red("Error generating image:"), file, e.message);
+    console.log(chalk.red('Error generating image:'), file, e.message);
   }
 }
