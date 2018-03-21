fd0837a8644c12065e2f943045a1ae96c8f66a4a
Mark Chambers <marksy@users.noreply.github.com>
2018-03-21 11:29:00 +0000

    added new spinner component (#152)
    * added new spinner component
    
    * added reference to design its based off of
    
    * changed the animation slightly to match GDS (extracted keyframes from their JS)
    
    * removed commented out bit

---
+++ packages/icons/src/Spinner/stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Button from '@govuk-react/button';
+
+import Spinner from '.';
+
+storiesOf('Icons', module).add('Spinner', () => (
+  <Spinner />
+)).add('Spinner (small)', () => (
+  <Spinner width="100px" height="100px" />
+)).add('Spinner inside button', () => (
+  <Button disabled><span style={{ display: 'flex', alignItems: 'center' }}>Saving<Spinner width="32px" height="32px" fill="white" /></span></Button>
+));
