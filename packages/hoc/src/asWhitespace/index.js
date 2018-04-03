897ae6e661b56b79bceaa8224a2c9f48b1c8166d
Mark Chambers <me@marksy.com>
2018-04-03 14:13:28 +0100

    wip

--- packages/hoc/src/asWhitespace/index.js
+++ packages/hoc/src/asWhitespace/index.js
@@ -2,6 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import glamorous from 'glamorous';
 import { BLUE } from 'govuk-colours';
+import {
+  RESPONSIVE_0,
+  RESPONSIVE_1,
+  RESPONSIVE_2,
+  RESPONSIVE_3,
+  RESPONSIVE_4,
+  RESPONSIVE_5,
+  RESPONSIVE_6,
+  RESPONSIVE_7,
+  RESPONSIVE_8,
+  RESPONSIVE_9,
+  MEDIA_QUERIES,
+} from '@govuk-react/constants';
+
+const spacingMap = [
+  RESPONSIVE_0,
+  RESPONSIVE_1,
+  RESPONSIVE_2,
+  RESPONSIVE_3,
+  RESPONSIVE_4,
+  RESPONSIVE_5,
+  RESPONSIVE_6,
+  RESPONSIVE_7,
+  RESPONSIVE_8,
+  RESPONSIVE_9,
+];
 
 const asWhitespace = (Component, marginBottom) => {
   const Whitespace = props => (
@@ -15,7 +41,10 @@ const asWhitespace = (Component, marginBottom) => {
 
   const StyledHoc = glamorous(Whitespace)({
     border: `1px solid ${BLUE}`,
-    marginBottom: marginBottom || 0,
+    marginBottom: spacingMap[marginBottom].mobile || 0,
+    [MEDIA_QUERIES.LARGESCREEN]: {
+      marginBottom: spacingMap[marginBottom].tablet || 0,
+    },
   });
 
   return StyledHoc;
