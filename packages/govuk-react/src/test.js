22420632f7663822b603588df11461f09398b27e
Alasdair McLeay <alasdair.mcleay@me.com>
2018-04-09 21:15:21 +0100

    failing unit test - all componenents are dependencies of govuk-react

--- packages/govuk-react/src/test.js
+++ packages/govuk-react/src/test.js
@@ -1,13 +1,25 @@
-// TODO: this does not currently test the purpose of src/index.js
-// It should:
-// - ensure all components are exported
-// - ensure all exports are components or functions
+import fs from 'fs';
+import path from 'path';
 
-import * as pkgs from './index';
+import * as packageExports from './index';
+import pkg from '../package.json';
 
 describe('Exports', () => {
   // all exports are truthy
-  Object.keys(pkgs).map(pkgName => it(`${pkgName} is truthy`, () => {
-    expect(!!pkgs[pkgName]).toBe(true);
+  Object.keys(packageExports).map(exportName => it(`${exportName} is truthy`, () => {
+    expect(!!packageExports[exportName]).toBe(true);
+  }));
+});
+
+// https://stackoverflow.com/questions/18112204/get-all-directories-within-directory-nodejs?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+const dirs = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory());
+
+describe('Components', () => {
+  // all componenents are dependencies of govuk-react
+  // if needed in future, can add an exclusion list/array to this test
+  const components = dirs(path.join(__dirname, '../../../components'));
+
+  components.map(component => it(`@govuk-react/${component} is in dependencies`, () => {
+    expect(!!pkg.dependencies[`@govuk-react/${component}`]).toBe(true);
   }));
 });
