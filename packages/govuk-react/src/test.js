c6d22c121b4063c6e58f5d1f7c60eb54ba40c739
Alasdair McLeay <penx@users.noreply.github.com>
2018-04-09 21:13:37 +0100

    Feature/check dependencies exports (#193)
    * Failing unit test for falsey exports
    
    * fix failing unit tests - header presets weren't exported from govuk-react/header

--- packages/govuk-react/src/test.js
+++ packages/govuk-react/src/test.js
@@ -2,17 +2,12 @@
 // It should:
 // - ensure all components are exported
 // - ensure all exports are components or functions
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-import { MultiChoice } from './index';
+import * as pkgs from './index';
 
-describe('MultiChoice', () => {
-  const example = 'example';
-  const wrapper = <MultiChoice label={example}>{example}</MultiChoice>;
-
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(wrapper, div);
-  });
+describe('Exports', () => {
+  // all exports are truthy
+  Object.keys(pkgs).map(pkgName => it(`${pkgName} is truthy`, () => {
+    expect(!!pkgs[pkgName]).toBe(true);
+  }));
 });
