dc6d17631e24c2b5a9a4e10358fe9cf2b77f5a2c
Mark Chambers <me@marksy.com>
2018-04-10 12:07:06 +0100

    removed topnav and fixed snapshot

--- components/top-nav/src/example.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import TopNav from '.';
-
-export default () => <TopNav>TopNav example</TopNav>;
--- components/top-nav/src/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import TopNav from '.';
-
-storiesOf('TopNav', module).add('TopNav', () => (
-  <TopNav>TopNav example</TopNav>
-));
