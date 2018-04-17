8adffa4b9d9e69a1a6a2d824c675b1959e17d535
Alasdair McLeay <penx@users.noreply.github.com>
2018-04-17 13:18:24 +0100

    Make storybook use src for components rather than lib (#211)
    Make storybook use src for components rather than lib.
    
    This means we don't need to run watch in the background separately, storybook will do this for us.
    
    Fixes #207
    
    * update storybook to 3.4.1

--- packages/storybook/.storybook/webpack.config.js
+++ packages/storybook/.storybook/webpack.config.js
@@ -1,19 +1,28 @@
 const path = require('path');
+const fs = require('fs');
 
-// load the default config generator.
-const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+const aliasToSrc = require('./aliasToSrc');
 
-module.exports = (baseConfig, env) => {
-  const config = genDefaultConfig(baseConfig, env);
+// https://stackoverflow.com/questions/18112204/get-all-directories-within-directory-nodejs?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+const dirs = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory()).map(f => path.join(p, f));
+
+module.exports = (baseConfig, env, defaultConfig) => {
 
   // Include PNGs imported from node_modules
-  config.module.rules.push({
+  defaultConfig.module.rules.push({
     test: /\.png/,
     loaders: ["url-loader"],
     include: path.resolve(__dirname, '../../..')
   });
-  // configuration for adding specific rules can be found here:
-  // https://storybook.js.org/configurations/custom-webpack-config/#full-control-mode
+  // make babel-loader process components and packages src folder
+  defaultConfig.module.rules[0].include.push(/\/(packages|components)\/[^/]+\/src/);
 
-  return config;
+  // when one component depends on another, we want to webpack to load from the component's src folder so that it auto refreshes when there are changes
+  defaultConfig.resolve.alias = {
+    ...defaultConfig.resolve.alias,
+    ...aliasToSrc(dirs('../../components')),
+    ...aliasToSrc(dirs('../../packages'))
+  }
+
+  return defaultConfig;
 };
--- packages/storybook/src/stories/index.js
+++ packages/storybook/src/stories/index.js
@@ -1,42 +1,4 @@
-// import { action } from '@storybook/addon-actions'
-// import { linkTo } from '@storybook/addon-links'
-
 import 'normalize.css';
 
-import '@govuk-react/back-link/lib/stories';
-import '@govuk-react/breadcrumb/lib/stories';
-import '@govuk-react/button/lib/stories';
-import '@govuk-react/checkbox/lib/stories';
-import '@govuk-react/date-input/lib/stories';
-import '@govuk-react/error-text/lib/stories';
-import '@govuk-react/file-upload/lib/stories';
-import '@govuk-react/grid-col/lib/stories';
-import '@govuk-react/grid-row/lib/stories';
-import '@govuk-react/header/lib/stories';
-import '@govuk-react/input-field/lib/stories';
-import '@govuk-react/layout/lib/stories';
-import '@govuk-react/panel/lib/stories';
-import '@govuk-react/radio/lib/stories';
-import '@govuk-react/select/lib/stories';
-import '@govuk-react/text-area/lib/stories';
-import '@govuk-react/phase-badge/lib/stories';
-import '@govuk-react/phase-banner/lib/stories';
-import '@govuk-react/search-box/lib/stories';
-import '@govuk-react/pagination/lib/stories';
-import '@govuk-react/ordered-list/lib/stories';
-import '@govuk-react/unordered-list/lib/stories';
-import '@govuk-react/list-item/lib/stories';
-import '@govuk-react/list-navigation/lib/stories';
-import '@govuk-react/related-items/lib/stories';
-import '@govuk-react/document-footer-metadata/lib/stories';
-import '@govuk-react/loading-box/lib/stories';
-import '@govuk-react/lead-paragraph/lib/stories';
-
-import '@govuk-react/hoc/lib/asAnchor/stories';
-import '@govuk-react/hoc/lib/asPaginationItem/stories';
-import '@govuk-react/hoc/lib/withWhiteSpace/stories';
-
-import '@govuk-react/icons/lib/ArrowLeft/stories';
-import '@govuk-react/icons/lib/ArrowRight/stories';
-import '@govuk-react/icons/lib/Search/stories';
-import '@govuk-react/icons/lib/Spinner/stories';
+const req = require.context('../../../../', true, /(packages|components)\/[^/]+\/src\/([^/]+\/)*stories.js$/);
+req.keys().forEach(req);
