048f1d41ce5e534f69b5683bf5a9f19dd1022ecd
Mark Chambers <me@marksy.com>
2018-04-17 21:27:53 +0100

    added snapshot and test coverage

--- components/table/src/test.js
+++ components/table/src/test.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
 import Table from './';
 import Row from './atoms/Row';
 import Cell from './atoms/Cell';
 
-const example = (
-  <Row><Cell>Example</Cell></Row>
+const example = <Table body={<Row><Cell>Example</Cell></Row>} />;
+const exampleWithHead = (
+  <Table
+    body={<Row><Cell>Example</Cell></Row>}
+    head={<Row><Cell>Example</Cell></Row>}
+  />
 );
+
 
 describe(Table, () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Table body={example} />, div);
+    ReactDOM.render(example, div);
+    ReactDOM.render(exampleWithHead, div);
+  });
+
+  it('matches snapshot for example', () => {
+    expect(mount(example)).toMatchSnapshot('example mount');
+  });
+
+  it('matches snapshot for exampleWithHead', () => {
+    expect(mount(exampleWithHead)).toMatchSnapshot('exampleWithHead mount');
   });
 });
