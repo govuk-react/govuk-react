a65ddd1fc21d9a694f3df1c89d40d191434a9ad3
Toby Brancher <email@tobybrancher.net>
2018-05-02 15:27:25 +0100

    Update LeadParagraph component from Glamorous to Emotion
    Added snapshot test

--- components/lead-paragraph/src/test.js
+++ components/lead-paragraph/src/test.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import { createSerializer } from 'jest-emotion';
+import * as emotion from 'emotion';
+
 import LeadParagraph from './';
 
+expect.addSnapshotSerializer(createSerializer(emotion));
+
 describe(LeadParagraph, () => {
+  const example = 'example';
+  const wrapper = <LeadParagraph>{example}</LeadParagraph>;
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<LeadParagraph>Example</LeadParagraph>, div);
+  });
+
+  it('matches wrapper snapshot', () => {
+    expect(mount(wrapper)).toMatchSnapshot('wrapper mount');
   });
 });
