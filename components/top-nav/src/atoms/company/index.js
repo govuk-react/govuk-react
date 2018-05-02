0af393645115f16be877d453b8386d47710d21eb
Alasdair McLeay <alasdair.mcleay@me.com>
2018-05-02 22:51:10 +0100

    remove cascade of styles in top nav

--- components/top-nav/src/atoms/company/index.js
+++ components/top-nav/src/atoms/company/index.js
@@ -1,6 +1,5 @@
 import glamorous from 'glamorous';
 import { WHITE } from 'govuk-colours';
-import { SPACING } from '@govuk-react/constants';
 
 const Company = glamorous.div({
   display: 'flex',
@@ -8,23 +7,6 @@ const Company = glamorous.div({
   alignItems: 'center',
   justifyContent: 'flex-start',
   fontSize: '30px',
-  ' a': {
-    display: 'flex',
-    color: WHITE,
-    textDecoration: 'none',
-    marginBottom: 0,
-    paddingBottom: 0,
-    borderBottom: '1px solid transparent',
-    ':hover': {
-      borderBottom: `1px solid ${WHITE}`,
-    },
-  },
-  ' h1': {
-    fontSize: '30px',
-  },
-  ' svg': {
-    marginRight: SPACING.SCALE_2,
-  },
 });
 
 export default Company;
