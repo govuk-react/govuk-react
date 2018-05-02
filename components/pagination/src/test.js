6d9b1a512f285aa523d37011d20f386bfa5680c4
Toby Brancher <email@tobybrancher.net>
2018-05-02 17:35:42 +0100

    Update Pagination component from Glamorous to Emotion
    Added basic snapshot test

--- components/pagination/src/test.js
+++ components/pagination/src/test.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createSerializer } from 'jest-emotion';
+import { mount } from 'enzyme';
+import * as emotion from 'emotion';
+
 import Pagination from './';
 
+expect.addSnapshotSerializer(createSerializer(emotion));
+
 describe(Pagination, () => {
+  const wrapper = <Pagination>Example</Pagination>;
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Pagination>Example</Pagination>, div);
+    ReactDOM.render(wrapper, div);
+  });
+
+  it('matches wrapper snapshot', () => {
+    expect(mount(wrapper)).toMatchSnapshot('wrapper mount');
   });
 });
