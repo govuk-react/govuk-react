b7815d1a553743fd9c005dcc6bad4ef98af79357
Alasdair McLeay <alasdair.mcleay@me.com>
2018-05-03 09:06:38 +0100

    remove cascade from nav rhs

--- components/top-nav/src/atoms/right-hand-side/index.js
+++ components/top-nav/src/atoms/right-hand-side/index.js
@@ -1,5 +1,4 @@
 import glamorous from 'glamorous';
-import { WHITE } from 'govuk-colours';
 import { MEDIA_QUERIES, SPACING } from '@govuk-react/constants';
 
 const RightHandSide = glamorous.div({
@@ -12,20 +11,6 @@ const RightHandSide = glamorous.div({
     width: '66.66%',
     paddingLeft: SPACING.SCALE_3,
     marginTop: 0,
-    ' button': {
-      display: 'none',
-    },
-  },
-  ' a': {
-    color: WHITE,
-    border: 0,
-    textDecoration: 'none',
-    borderBottom: '1px solid transparent',
-    marginBottom: 0,
-    paddingBottom: 0,
-    ':hover': {
-      borderBottom: `1px solid ${WHITE}`,
-    },
   },
 });
 
