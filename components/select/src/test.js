ad9acc3300461a67b7ec60ceb32eebf60f6e6489
Toby Brancher <email@tobybrancher.net>
2018-05-03 11:25:29 +0100

    Update Select component from Glamorous to Emotion

--- components/select/src/test.js
+++ components/select/src/test.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { mount } from 'enzyme';
+import { createSerializer } from 'jest-emotion';
+import * as emotion from 'emotion';
+
 import Select from './';
+
+expect.addSnapshotSerializer(createSerializer(emotion));
 
 describe('Select', () => {
   const example = 'example';
