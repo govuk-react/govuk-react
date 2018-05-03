a6027fd671b729fc10624abc5b0b1b983226a912
Alasdair McLeay <alasdair.mcleay@me.com>
2018-05-03 09:26:06 +0100

    comments on adjacent sibling selector and form ids

--- components/top-nav/src/atoms/menu-button/index.js
+++ components/top-nav/src/atoms/menu-button/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import glamorous from 'glamorous';
 import { MEDIA_QUERIES } from '@govuk-react/constants';
 
-const ControlledLabel = glamorous.label({
+const Label = glamorous.label({
   [MEDIA_QUERIES.LARGESCREEN]: {
     display: 'none',
   },
 });
 
-const ControlledInput = glamorous.input({
+const Input = glamorous.input({
   position: 'absolute',
   top: '-999em',
+  // This is reliant on HTML structure of parent component.
+  // If we wanted to create a React Native version of this component,
+  // we wouldn't be able to rely on HTML syntax for styles.
+  // For React Native we would want to lift up state to
+  // parent component and/or use context API.
+  // However, using this technique, the navigation can be used when
+  // server side rendered and with JavaScript disabled.
+  // As we are in control of the HTML structure of the parent component,
+  // and we do not plan to make a React Native version of this component at
+  // the moment, this is ok.
   ':checked + ul': {
     display: 'flex',
   },
@@ -19,13 +30,26 @@ const ControlledInput = glamorous.input({
   },
 });
 
-const MenuButton = () => (
+const MenuButton = ({ title }) => (
   <React.Fragment>
-    <ControlledLabel htmlFor="dingus">
-      Menu <span>&rsaquo;</span>
-    </ControlledLabel>
-    <ControlledInput id="dingus" type="checkbox" />
+    {/*
+      We can't nest the Input inside the label as we are using the
+      adjacent sibling selector to expand and collapse the nav.
+      We need to use an ID, and can be fairly suer that there would
+      only ever be one instance of this component on the page.
+    */}
+    <Label htmlFor="govuk-react-menu-button">
+      {title} <span>&rsaquo;</span>
+    </Label>
+    <Input id="govuk-react-menu-button" type="checkbox" />
   </React.Fragment>
 );
+
+MenuButton.propTypes = {
+  title: PropTypes.string,
+};
+MenuButton.defaultProps = {
+  title: 'Menu',
+};
 
 export default MenuButton;
