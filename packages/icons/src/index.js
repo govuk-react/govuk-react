3215eae40b8b6aef51a504ee3be8809f9ef3317f
Alasdair McLeay <penx@users.noreply.github.com>
2018-05-04 14:18:58 +0100

    export svgbase so it can be used by other projects (#240)
    * export svgbase so it can be used by other projects
    
    * glamor should be in icons peer dependency to avoid dupes

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -1,5 +1,6 @@
-export { default as ArrowLeft } from './ArrowLeft';
-export { default as ArrowRight } from './ArrowRight';
-export { default as Search } from './Search';
-export { default as Spinner } from './Spinner';
-export { default as ButtonArrow } from './ButtonArrow';
+export ArrowLeft from './ArrowLeft';
+export ArrowRight from './ArrowRight';
+export Search from './Search';
+export Spinner from './Spinner';
+export ButtonArrow from './ButtonArrow';
+export SVGBase from './SVGBase';
