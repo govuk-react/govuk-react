435a7cad2b35ba76453719a016e628aa066049e9
Toby Brancher <email@tobybrancher.net>
2018-05-09 17:06:13 +0100

    Add `viewBox` attribute to arrow icon assets
    This is required to ensure the SVG assets can scale

--- packages/icons/src/ArrowLeft/index.js
+++ packages/icons/src/ArrowLeft/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import SVG from '../SVGBase/index';
 
 const ArrowLeft = ({ fill, title, ...rest }) => (
-  <SVG title={title} fill={fill} {...rest}>
+  <SVG viewBox="0 0 17 14" title={title} fill={fill} {...rest}>
     <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z" />
   </SVG>
 );
