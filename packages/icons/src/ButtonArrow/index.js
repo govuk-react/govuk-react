db3cf11d26e33c73b01584e20b436afb05ee0365
Toby Brancher <email@tobybrancher.net>
2018-05-11 15:12:36 +0100

    Feature/tidy svg (#250)
    * Remove default viewbox attribute
    
    The viewbox attribute should be icon specific and should not have a generic default.
    
    Fixes #238
    
    * Improve SVG icon stories
    
     - Added sensible default width with auto height
     - Added knobs for fill, width and height
    
    * Improve ButtonArrow SVG asset
    
    Remove transform and optimise with SVGOMG
    
    * Add `viewBox` attribute to arrow icon assets
    
    This is required to ensure the SVG assets can scale
    
    * Set default fill colour in storybook on ArrowLeft from Red to Black
    
    * Remove un-required default SVG attributes from SVGBase
    
    - XMLNS is not required as part of the spec or observed by browsers: https://www.w3.org/TR/html5/
    - `x` and `y` co-ordinates default to 0 without being defined
    - `width` and `height` will scale to fit container if not added
    - `className` is optional and can be included as part of `…rest`
    - defaulting to `display: block` could be misleading and should be added on a case-by-case basis
    
    * Update Icon snapshots
    
    * Fix lint errors
    
    * Restore original defaults for width, height and display
    
    Restoring `width`, `height` and `display` defaults as it allows for the current icon implementations to be simpler and should suffice for any new SVG assets being added.
    
    * Test chromatic with different story defaults
    
    * Resolve chromatic rendering blank snapshots for SVG icons
    
    For some reason using numbers with no type definition causes chromatic snapshots to render blank whilst the “view live” link displays the story as expected and visible in storybook. This solution replaces the `number` “knobs” to `text` with a format definition (in this case %, but could be px/em/etc…)
    
    * Update snapshots to reflect the updated SVGBase
    
    * Offset viewbox on LeftArrow to avoid truncating the path
    
    * Update snapshot to represent viewbox adjustment on LeftArrow

--- packages/icons/src/ButtonArrow/index.js
+++ packages/icons/src/ButtonArrow/index.js
@@ -6,8 +6,8 @@ import SVG from '../SVGBase/index';
 const ButtonArrow = ({ fill, title, ...rest }) => (
   <SVG viewBox="0 0 706 860" title={title} fill={fill} {...rest}>
     <g>
-      <polygon points="0.15234375 0 253.145018 0 705.25342 430 452.260746 430" />
-      <polygon fillOpacity="0.5" transform="translate(352.550538, 645.000000) scale(1, -1) translate(-352.550538, -645.000000) " points="0 430 252.992674 430 705.101076 860 452.108402 860" />
+      <path d="M.152 0h252.993l452.108 430H452.261z" />
+      <path fillOpacity="0.5" d="M0 860h252.993L705.1 430H452.108z" />
     </g>
   </SVG>
 );
--- packages/icons/src/SVGBase/index.js
+++ packages/icons/src/SVGBase/index.js
@@ -2,19 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SVG = ({
-  children, fill, title, className, ...rest
+  children, fill, title, ...rest
 }) => (
   <svg
     version="1.1"
-    xmlns="http://www.w3.org/2000/svg"
-    x="0px"
-    y="0px"
+    fill={fill}
     width="100%"
     height="100%"
-    className={className}
     style={{ display: 'block' }}
-    fill={fill}
-    viewBox="-0.2 0 17 14" // TODO: #238 why?
     {...rest}
   >
     <title>{title}</title>
@@ -24,13 +19,11 @@ const SVG = ({
 
 SVG.propTypes = {
   children: PropTypes.node.isRequired,
-  className: PropTypes.string,
   title: PropTypes.string,
   fill: PropTypes.string,
 };
 
 SVG.defaultProps = {
-  className: undefined,
   title: undefined,
   fill: 'currentColor',
 };
