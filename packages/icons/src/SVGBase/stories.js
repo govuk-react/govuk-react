72edb4ed676b4fcd4323062ec0c13c74d75c3a0b
Toby Brancher <email@tobybrancher.net>
2018-05-14 14:52:13 +0100

    Story and example for SVGBase

---
+++ packages/icons/src/SVGBase/stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, color, text, object } from '@storybook/addon-knobs/react';
+
+import SVGBase from '.';
+
+const stories = storiesOf('Icons', module);
+stories.addDecorator(withKnobs);
+
+stories.add('SVGBase', () => {
+  const fill = color('fill', 'transparent');
+  const stroke = color('stroke', 'orange');
+  const viewBox = text('viewBox', '0 0 500 500');
+  return (
+    <SVGBase viewBox={viewBox} title="Example of SVGBase" fill={fill} stroke={stroke}>
+      <polygon points="250,60 100,400 400,400" />
+    </SVGBase>
+  );
+});
+
