b0d4f80455dc1ba0d899f1b343833668a7f1c377
Toby Brancher <email@tobybrancher.net>
2018-05-14 11:39:43 +0100

    Remove blank Storybook entry
    Un-required storybook entry for SVGBase. I am not sure if it is needed in storybook, or can I think of a good way of representing it in Storybook. Will remove for the moment to avoid any confusion.

--- packages/icons/src/SVGBase/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import SVG from '.';
-
-storiesOf('Search', module).add('Search', () => (
-  <SVG title="example">SVG example</SVG>
-));
