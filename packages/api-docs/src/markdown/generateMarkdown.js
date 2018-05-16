356c483f5c74d469c2741f0a047187dbd1777d1d
Toby Brancher <email@tobybrancher.net>
2018-05-16 17:11:40 +0100

    Quickly swap out image snapshots for live storybook examples
     - Remove `generateComponentImage` from api-doc generation
     - Add `getComponentFolderName`
     - Test modifications to `generateMarkdown` for storybook-readme

--- packages/api-docs/src/markdown/generateMarkdown.js
+++ packages/api-docs/src/markdown/generateMarkdown.js
@@ -2,11 +2,16 @@ import generateDescription from './generateDescription';
 import generateTitle from './generateTitle';
 import generateProps from './generateProps';
 
-export default function generateMarkdown(name, reactAPI, imagePath) {
-  const markdownString = `${generateTitle(name)}\n${generateDescription(
-    reactAPI.description,
-    imagePath,
-  )}\n${generateProps(reactAPI.props)}\n`;
-
+export default function generateMarkdown(name, folderName, reactAPI) {
+  const markdownString = `${generateTitle(name)}
+### Import
+\`\`\`js
+  import ${name} from '@govuk-react/${folderName}';
+\`\`\`
+<!-- STORY -->
+${generateDescription(reactAPI.description)}
+### Properties
+${generateProps(reactAPI.props)}
+`;
   return markdownString;
 }
