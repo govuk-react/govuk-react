0c8f850046302fe67f0bb585dc4b1ffa4fa61043
Toby Brancher <email@tobybrancher.net>
2018-05-22 16:39:57 +0100

    Add eslint rules for react-router `Link` components attribute `to`

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,12 @@ module.exports = {
   "rules": {
     "react/jsx-filename-extension": 0,
     "jsx-a11y/label-has-for": 0,
-    "filenames/match-exported": 0
+    "filenames/match-exported": 0,
+    "jsx-a11y/anchor-is-valid": [ "error", {
+      "components": [ "Link" ],
+      "specialLink": [ "to", "hrefLeft", "hrefRight" ],
+      "aspects": [ "noHref", "invalidHref", "preferButton" ]
+    }]
   },
   "overrides": [
     {
