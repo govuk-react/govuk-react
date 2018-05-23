804b7becf2f85171231a999764a98d87d8add744
Toby Brancher <email@tobybrancher.net>
2018-05-23 16:26:01 +0100

    Update fixture text

--- components/paragraph/src/fixtures.js
+++ components/paragraph/src/fixtures.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { text, boolean } from '@storybook/addon-knobs/react';
 import Paragraph from '.';
 
-const exampleText = 'A 19px body copy paragraph. [This supports markdown](https://en.wikipedia.org/wiki/Markdown) to allow for **bold** text and *italics*. This includes even more text to give a good representation of a more average length paragraph. That way you can see more than one line wrapping.';
-const exampleSupportingText = 'A 16px supporting text paragraph. This supports markdown to allow for **bold** text and *italics*. This includes even more text to give a good representation of a more average length paragraph. That way you can see more than one line wrapping.';
+const exampleText = 'A 19px body copy paragraph. This supports markdown to allow for **bold** text and *italics*, as well as [links](https://en.wikipedia.org/wiki/Markdown). This includes even more text to give a good representation of a more average length paragraph. That way you can see more than one line wrapping.';
+const exampleSupportingText = 'A 16px supporting text paragraph. This supports markdown to allow for **bold** text and *italics*, as well as [links](https://en.wikipedia.org/wiki/Markdown). This includes even more text to give a good representation of a more average length paragraph. That way you can see more than one line wrapping.';
 
 export default () => <Paragraph supportingText={boolean('supportingText', false)}>{text('children', exampleText)}</Paragraph>;
 
