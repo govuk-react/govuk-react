caa4418abdec1ef1bc6e8079789dd369a4fc3996
Toby Brancher <email@tobybrancher.net>
2018-05-24 11:11:27 +0100

    Make LinkRenderer an arrow function

--- components/paragraph/src/link-renderer.js
+++ components/paragraph/src/link-renderer.js
@@ -5,13 +5,11 @@ import Anchor from './atoms/anchor';
 
 // Support react-router links
 // https://github.com/rexxars/react-markdown/issues/29#issuecomment-231556543
-function LinkRenderer({ href, children }) {
-  return (
-    href.match(/^\//)
-      ? <Link to={href}>{children}</Link>
-      : <Anchor href={href}>{children}</Anchor>
-  );
-}
+const LinkRenderer = ({ href, children }) => (
+  href.match(/^\//)
+    ? <Link to={href}>{children}</Link>
+    : <Anchor href={href}>{children}</Anchor>
+);
 
 LinkRenderer.propTypes = {
   href: PropTypes.string,
