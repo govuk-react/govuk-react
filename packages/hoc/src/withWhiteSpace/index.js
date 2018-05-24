7e65535e6d1a32ce7fea82412535cd029d7fc4e5
Tarandeep Singh Chauhan <Tarandeep.Chauhan-CIC-UK@ibm.com>
2018-05-24 16:36:58 +0100

    Problem - withWhitespace was overwriting the marginBottom on the Headings causing the margins to be removed
    Solution:
    - Refactored withWhitespace to only apply when marginBottom is passed in as a prop.

--- packages/hoc/src/withWhiteSpace/index.js
+++ packages/hoc/src/withWhiteSpace/index.js
@@ -2,15 +2,17 @@ import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 import { SPACING_MAP, SPACING_MAP_INDEX, MEDIA_QUERIES } from '@govuk-react/constants';
 
-const withWhiteSpace = config => (Component) => {
-  const StyledHoc = styled(Component)(({
-    mb: marginBottom = config.marginBottom,
-  }) => ({
-    marginBottom: marginBottom ? SPACING_MAP[marginBottom].mobile : 0,
-    [MEDIA_QUERIES.LARGESCREEN]: {
-      marginBottom: marginBottom ? SPACING_MAP[marginBottom].tablet : 0,
-    },
-  }));
+const withWhiteSpace = (config = {}) => (Component) => {
+  const StyledHoc = styled(Component)(({ mb: marginBottom = config.marginBottom }) => (
+    marginBottom &&
+      {
+        marginBottom: marginBottom ? SPACING_MAP[marginBottom].mobile : 0,
+        [MEDIA_QUERIES.LARGESCREEN]: {
+          marginBottom: marginBottom ? SPACING_MAP[marginBottom].tablet : 0,
+        },
+      }
+  ));
+
 
   // `mb` (Margin Bottom) prop name comes from the naming convention used by https://github.com/jxnblk/grid-styled
   StyledHoc.propTypes = {
@@ -21,3 +23,4 @@ const withWhiteSpace = config => (Component) => {
 };
 
 export default withWhiteSpace;
+
