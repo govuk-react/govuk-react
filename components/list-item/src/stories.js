551dc4d903b311a2c7d59120b13c2bc1dc0c5926
Toby Brancher <email@tobybrancher.net>
2018-05-29 13:54:19 +0100

    Update list-item stories to support readme and knobs

--- components/list-item/src/stories.js
+++ components/list-item/src/stories.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs/react';
+import { withDocs } from 'storybook-readme';
 
 import ListItem from '.';
+import ReadMe from '../README.md';
 
 const stories = storiesOf('Typography/ListItem', module);
 
+stories.addDecorator(withDocs(ReadMe));
+stories.addDecorator(withKnobs);
+
 stories.add('ListItem', () => (
-  <ListItem>ListItem example</ListItem>
+  <ListItem>{text('Children', 'List item example')}</ListItem>
 ));
