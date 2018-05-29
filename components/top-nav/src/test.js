3d3469eac38cb603031c85c5bb03949e36c710d7
Tarandeep Singh Chauhan <Tarandeep.Chauhan-CIC-UK@ibm.com>
2018-05-29 11:38:09 +0100

    - Implement tests for NavLinkAnchor and LogoAnchor - Regenerate snapshots

--- components/top-nav/src/test.js
+++ components/top-nav/src/test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { mount } from 'enzyme';
 import CrownIcon from '@govuk-react/icon-crown';
 
-import TopNav, { asAnchor } from './';
+import TopNav, { asAnchor, LogoAnchor, NavLinkAnchor } from './';
 
 const emptyNode = [];
 const nullNode = null;
@@ -15,6 +15,19 @@ const wrapperMultiple = (
     <Anchor href="/section">Section 1</Anchor>
   </TopNav>
 );
+
+const wrapperLogoAnchor = (
+  <TopNav>
+    <LogoAnchor href="/logo-link">Logo link</LogoAnchor>
+  </TopNav>
+);
+
+const wrapperNavLinkAnchor = (
+  <TopNav>
+    <NavLinkAnchor href="/nav-link">Nav link</NavLinkAnchor>
+  </TopNav>
+);
+
 const wrapperEmptyNode = (
   <TopNav>
     <Anchor href="/section">Section 1</Anchor>
@@ -23,6 +36,10 @@ const wrapperEmptyNode = (
     example
   </TopNav>
 );
+
+const WrapperLogoAnchor = mount(wrapperLogoAnchor);
+const WrapperNavLinkAnchor = mount(wrapperNavLinkAnchor);
+const WrapperMultiple = mount(wrapperMultiple);
 
 describe(TopNav, () => {
   it('renders without crashing', () => {
@@ -34,8 +51,33 @@ describe(TopNav, () => {
     expect(mount(wrapperEmptyNode).find('ul li')).toHaveLength(2);
   });
 
-  it('matches snapshot', () => {
-    expect(mount(wrapperMultiple)).toMatchSnapshot('enzyme.mount');
+  it('matches the <TopNav> with multiple <Anchor> tags snapshot', () => {
+    expect(WrapperMultiple).toMatchSnapshot('enzyme.mount');
+  });
+
+  it('matches the props passed to the list of Anchor tags', () => {
+    WrapperMultiple.find(Anchor).forEach((anchor) => {
+      expect(anchor.props().href).toEqual('/section');
+      expect(anchor.props().children).toEqual('Section 1');
+    });
+  });
+
+  it('matches the <TopNav> with a <LogoAnchor> tag snapshot', () => {
+    expect(WrapperLogoAnchor).toMatchSnapshot('enzyme.mount');
+  });
+
+  it('matches the props passed to LogoAnchor', () => {
+    expect(WrapperLogoAnchor.find(LogoAnchor).props().href).toEqual('/logo-link');
+    expect(WrapperLogoAnchor.find(LogoAnchor).props().children).toEqual('Logo link');
+  });
+
+  it('matches the <TopNav> with a <NavLinkAnchor> tag snapshot', () => {
+    expect(WrapperNavLinkAnchor).toMatchSnapshot('enzyme.mount');
+  });
+
+  it('matches the props passed to NavLinkAnchor', () => {
+    expect(WrapperNavLinkAnchor.find(NavLinkAnchor).props().href).toEqual('/nav-link');
+    expect(WrapperNavLinkAnchor.find(NavLinkAnchor).props().children).toEqual('Nav link');
   });
 
   it('with icon title', () => {
