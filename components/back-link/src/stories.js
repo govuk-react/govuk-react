f753d6d33725c0ae855a2eaef32a386b468bf605
Toby Brancher <email@tobybrancher.net>
2018-05-29 09:25:49 +0100

    Readme link for back-link

--- components/back-link/src/stories.js
+++ components/back-link/src/stories.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withDocs } from 'storybook-readme';
+
 import BackLink from '.';
+import ReadMe from '../README.md';
 
 const stories = storiesOf('Navigation/BackLink', module);
+
+stories.addDecorator(withDocs(ReadMe));
 
 stories.add('Component default', () => (
   <BackLink goBack={action('go-back')}>Back</BackLink>
