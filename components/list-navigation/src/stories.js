c7731c683191e0ae0e891dcefa44263d49b84fb9
Toby Brancher <email@tobybrancher.net>
2018-05-29 15:22:09 +0100

    Update list-navigation stories to support readme

--- components/list-navigation/src/stories.js
+++ components/list-navigation/src/stories.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { asAnchor } from '@govuk-react/hoc';
+import { WithDocsCustom } from '@govuk-react/storybook-components';
 
 import ListNavigation from '.';
+import ReadMe from '../README.md';
 
 const AnchorTag = asAnchor('a');
 
 const stories = storiesOf('Navigation/ListNavigation', module);
+
+stories.addDecorator(WithDocsCustom(ReadMe));
 
 stories.add('ListNavigation', () => (
   <ListNavigation listStyleType="square">
