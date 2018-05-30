98937968e99bd3057432f033705fc131dfaf170e
Toby Brancher <email@tobybrancher.net>
2018-05-30 17:13:19 +0100

    Add a bit more detail to the path

--- packages/api-docs/src/index.js
+++ packages/api-docs/src/index.js
@@ -57,7 +57,7 @@ async function generateApiForFile(file) {
 }
 
 function shouldDocumentComponent(file) {
-  // only document components that are exported from src/index.js
+  // only document components that are exported from packages/govuk-react/src/index.js
   const name = getComponentNameFromFile(file);
   return Object.prototype.hasOwnProperty.call(components, name);
 }
