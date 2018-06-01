5266e6fd3275615bd19eb6b7d354741de64e3507
Toby Brancher <email@tobybrancher.net>
2018-06-01 12:30:28 +0100

    Remove un-required story examples from breadcrumb
    Due to the update with embedded docs, separate examples are not required for breadcrumbs and could be confusing/misleading.

--- components/breadcrumb/src/stories.js
+++ components/breadcrumb/src/stories.js
@@ -1,49 +1,18 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { BrowserRouter, Link } from 'react-router-dom';
-import { asAnchor } from '@govuk-react/hoc';
 import { WithDocsCustom } from '@govuk-react/storybook-components';
 
 import Breadcrumb from '.';
 import ReadMe from '../README.md';
 
-const AnchorTag = asAnchor('a');
-const AnchorLink = asAnchor(Link);
-
-const crumbsWithDuffChildren = [];
-
 const stories = storiesOf('Navigation/Breadcrumb', module);
-const examples = storiesOf('Navigation/Breadcrumb/Examples', module);
 
 stories.addDecorator(WithDocsCustom(ReadMe));
 
 stories.add('Component default', () => (
   <Breadcrumb>
-    <AnchorTag href="/section">Section 1</AnchorTag>
+    <a href="/section">Section</a>
     <a href="/section/sub-section">Sub-section</a>
     Current page
   </Breadcrumb>
 ));
-
-examples.add('Three levels deep', () => (
-  <Breadcrumb>
-    <a href="/section">Section 1</a>
-    <a href="/section/sub-section">Sub-section</a>
-    Current page
-  </Breadcrumb>
-));
-
-examples.add(
-  'Three levels deep using React Router Link',
-  () => (
-    <Breadcrumb>
-      <BrowserRouter>
-        <AnchorLink to="/section">Section 1</AnchorLink>
-      </BrowserRouter>
-      <BrowserRouter>
-        <AnchorLink to="/section/sub-section">Sub-section</AnchorLink>
-      </BrowserRouter>
-      Current page
-    </Breadcrumb>
-  ),
-);
