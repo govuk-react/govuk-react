0769b5abf72c1be3a68282e0c7a3f3aabfc52d56
Toby Brancher <email@tobybrancher.net>
2018-06-01 15:57:50 +0100

    Add example using asAnchor to breadcrumb

--- components/breadcrumb/src/index.js
+++ components/breadcrumb/src/index.js
@@ -82,6 +82,20 @@ const BreadcrumbListItem = styled('li')({
  * </Breadcrumb>
  * ```
  *
+ * Using `asAnchor` HOC with, or without React Router
+ * ```jsx
+ * import { Link } from 'react-router-dom';
+ * import { asAnchor } from '@govuk-react/hoc';
+ *
+ * const AnchorLink = asAnchor(Link);
+ * const AnchorTag = asAnchor('a');
+ *
+ * <Breadcrumb>
+ *   <AnchorLink to="/section">Section</AnchorLink>
+ *   <AnchorTag href="/section">Sub-section</AnchorTag>
+ * </Breadcrumb>
+ * ```
+ *
  * ### References:
  * - https://github.com/alphagov/govuk-frontend/blob/master/src/components/breadcrumb/_breadcrumb.scss
  *
