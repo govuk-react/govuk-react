4019574836b419afef4a1f4b770e52402476c579
Tarandeep Singh Chauhan <Tarandeep.Chauhan-CIC-UK@ibm.com>
2018-06-07 17:14:42 +0100

    Set up docs to generate in Storybook

--- components/error-summary/src/stories.js
+++ components/error-summary/src/stories.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { WithDocsCustom } from '@govuk-react/storybook-components';
 
 import ErrorSummary from '.';
+import ReadMe from '../README.md';
 
-storiesOf('ErrorSummary', module).add('ErrorSummary', () => (
+const stories = storiesOf('Form/ErrorSummary', module);
+
+stories.addDecorator(WithDocsCustom(ReadMe));
+
+stories.add('Component default', () => (
   <ErrorSummary>ErrorSummary example</ErrorSummary>
 ));
