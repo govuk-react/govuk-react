f3f1bf5b25864a2c2109a5968733a3d4b788ac08
Toby Brancher <email@tobybrancher.net>
2018-06-08 19:37:54 +0100

    Allow defaultProp `objects` render in api-docs
    Markdown tables don’t support multiline cells, meaning defaultTypes such as objects, that were returned on multiple lines would break. This change removes line-returns and carriage-returns from defaultProp values ensuring `objects` render on one line and in our documentation. yey!

--- packages/api-docs/src/markdown/generatePropDefaultValue.js
+++ packages/api-docs/src/markdown/generatePropDefaultValue.js
@@ -1,3 +1,8 @@
 export default function generatePropDefaultValue(value) {
-  return `${value.value}`;
+  /**
+   * Format default props to be on one line in order
+   * to render vartypes like objects in Markdown tables
+   */
+  const formattedValue = value.value.replace(/\n|\r/g, ' ');
+  return `${formattedValue}`;
 }
