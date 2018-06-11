197006d1b1da016e5d22a8b0a9ff7da91effc8a3
Tarandeep Singh Chauhan <Tarandeep.Chauhan-CIC-UK@ibm.com>
2018-06-11 16:34:33 +0100

    Add inline documentation of example usage

--- components/error-summary/src/index.js
+++ components/error-summary/src/index.js
@@ -1,5 +1,3 @@
-// TODO INSERT A COMMENT REFERENCE TO EXTERNAL URL IF POSSIBLE
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
@@ -65,6 +63,58 @@ const StyledErrorSummary = styled('div')({
   },
 });
 
+/**
+ *
+ * ### Usage
+ *
+ * Simple
+ * ```jsx
+ * export const heading = 'Message to alert the user to a problem goes here';
+ * export const description = 'Optional description of the errors and how to correct them';
+ *
+ * const StyledDiv = styled('div')({
+ *   marginBottom: '150px',
+ * });
+ *
+ * export default class ErrorSummaryExample extends React.Component {
+ *   errors = [
+ *     {
+ *       id: 0,
+ *       handleScrollToElement: () => this.targetQuestion.scrollIntoView(),
+ *       text: 'Descriptive link to the target question with an error',
+ *     },
+ *     {
+ *       id: 1,
+ *       handleScrollToElement: () => this.otherTargetQuestion.scrollIntoView(),
+ *       text: 'Descriptive link to the other target question with an error',
+ *     },
+ *   ];
+ *
+ *   render() {
+ *     return (
+ *       <div>
+ *         <ErrorSummary
+ *           heading={heading}
+ *           description={description}
+ *           errors={this.errors}
+ *         />
+ *         <StyledDiv innerRef={(node) => { this.targetQuestion = node; }}>
+ *           Target Question
+ *         </StyledDiv>
+ *         <StyledDiv innerRef={(node) => { this.otherTargetQuestion = node; }}>
+ *           Other Target Question
+ *         </StyledDiv>
+ *       </div>
+ *     );
+ *   }
+ * }
+ * ```
+ *
+ * ### References:
+ * - https://govuk-elements.herokuapp.com/errors/#summarise-errors
+ * - https://github.com/alphagov/govuk-frontend/tree/master/src/components/error-summary
+ *
+ */
 const ErrorSummary = ({
   heading, description, errors, ...props
 }) => (
