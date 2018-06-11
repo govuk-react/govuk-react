e8061fc40b6b60ba7b5a01e19efc2e6039a97df3
Tarandeep Singh Chauhan <Tarandeep.Chauhan-CIC-UK@ibm.com>
2018-06-11 12:26:32 +0100

    ErrorSummary without dynamic props

--- components/error-summary/src/index.js
+++ components/error-summary/src/index.js
@@ -4,33 +4,75 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 
+import { NTA_LIGHT, TEXT_COLOUR, ERROR_COLOUR, FOCUS_COLOUR } from 'govuk-colours';
+
+import Header from '@govuk-react/header';
+import Paragraph from '@govuk-react/paragraph';
+import UnorderedList from '@govuk-react/unordered-list';
+import ListItem from '@govuk-react/list-item';
+
 import {
   FONT_SIZE,
+  BORDER_WIDTH,
+  BORDER_WIDTH_MOBILE,
+  FOCUS_WIDTH,
   LINE_HEIGHT,
+  SPACING,
   MEDIA_QUERIES,
-  NTA_LIGHT,
+  RESPONSIVE_4,
 } from '@govuk-react/constants';
 
-const ErrorSummaryInner = styled('div')({
-  boxSizing: 'border-box',
+import { asAnchor, withWhiteSpace } from '@govuk-react/hoc';
+
+const AnchorTag = asAnchor('a');
+
+const StyledErrorText = styled(AnchorTag)({
   fontFamily: NTA_LIGHT,
-  fontWeight: 400,
+  WebkitFontSmoothing: 'antialiased',
+  MozOsxFontSmoothing: 'grayscale',
+  fontWeight: 700,
+  marginBottom: SPACING.SCALE_1,
+  textDecoration: 'underline',
+  textDecorationSkipInk: 'none',
   textTransform: 'none',
-  fontSize: FONT_SIZE.SIZE_14,
-  lineHeight: LINE_HEIGHT.SIZE_14,
-  width: '100%',
+  cursor: 'pointer',
+  fontSize: FONT_SIZE.SIZE_16,
+  lineHeight: LINE_HEIGHT.SIZE_16,
+  ':visited': {
+    color: `${ERROR_COLOUR}`,
+  },
   [MEDIA_QUERIES.LARGESCREEN]: {
-    fontSize: FONT_SIZE.SIZE_16,
-    lineHeight: LINE_HEIGHT.SIZE_16,
+    fontSize: FONT_SIZE.SIZE_19,
+    lineHeight: LINE_HEIGHT.SIZE_19,
+  },
+  color: `${ERROR_COLOUR}`,
+  paddingTop: '4px',
+  paddingBottom: '2px',
+});
+
+const StyledErrorSummary = styled('div')({
+  color: TEXT_COLOUR,
+  padding: RESPONSIVE_4.mobile,
+  border: `${BORDER_WIDTH_MOBILE} solid ${ERROR_COLOUR}`,
+  '&:focus': {
+    outline: `${FOCUS_WIDTH} solid ${FOCUS_COLOUR}`,
+    outlineOffset: '0',
+  },
+  [MEDIA_QUERIES.LARGESCREEN]: {
+    padding: RESPONSIVE_4.tablet,
+    border: `${BORDER_WIDTH} solid ${ERROR_COLOUR}`,
   },
 });
 
-const ErrorSummary = ({ children }) => (
-  <ErrorSummaryInner>{children}</ErrorSummaryInner>
+const ErrorSummary = props => (
+  <StyledErrorSummary tabIndex={-1} {...props}>
+    <Header level={2}>Message to alert the user to a problem goes here</Header>
+    <Paragraph mb={6}>Optional description of the errors and how to correct them</Paragraph>
+    <UnorderedList listStyleType="none">
+      <ListItem><StyledErrorText href="#target">Descriptive link to the question with an error</StyledErrorText></ListItem>
+      <ListItem><StyledErrorText href="#target">Descriptive link to the question with an error</StyledErrorText></ListItem>
+    </UnorderedList>
+  </StyledErrorSummary>
 );
 
-ErrorSummary.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-export default ErrorSummary;
+export default withWhiteSpace({ marginBottom: 6 })(ErrorSummary);
--- components/error-summary/src/stories.js
+++ components/error-summary/src/stories.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { WithDocsCustom } from '@govuk-react/storybook-components';
+import styled from 'react-emotion';
 
 import ErrorSummary from '.';
 import ReadMe from '../README.md';
+
+const StyledDiv = styled('div')({
+  marginBottom: '600px',
+});
 
 const stories = storiesOf('Form/ErrorSummary', module);
 
 stories.addDecorator(WithDocsCustom(ReadMe));
 
 stories.add('Component default', () => (
-  <ErrorSummary>ErrorSummary example</ErrorSummary>
+  <div>
+    <ErrorSummary>ErrorSummary example</ErrorSummary>
+    <StyledDiv id="target">Target Question</StyledDiv>
+  </div>
 ));
