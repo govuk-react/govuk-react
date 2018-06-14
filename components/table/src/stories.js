e9e34b0af0ae5886f70bd9bba819ed6b8750ab03
Tarandeep Singh Chauhan <Tarandeep.Chauhan-CIC-UK@ibm.com>
2018-06-14 12:06:59 +0100

    Update stories to use Table.Row, Table.Cell and Table.CellHeader syntax

--- components/table/src/stories.js
+++ components/table/src/stories.js
@@ -3,62 +3,59 @@ import { storiesOf } from '@storybook/react';
 import { WithDocsCustom } from '@govuk-react/storybook-components';
 
 import Table from '.';
-import CellHeader from './atoms/CellHeader';
-import Cell from './atoms/Cell';
-import Row from './atoms/Row';
 import ReadMe from '../README.md';
 
 const example1Body = (
   <React.Fragment>
-    <Row>
-      <CellHeader>First 6 weeks</CellHeader>
-      <Cell>£109.80 per week</Cell>
-    </Row>
-    <Row>
-      <Cell>Next 33 weeks</Cell>
-      <Cell>£109.80 per week</Cell>
-    </Row>
-    <Row>
-      <Cell>Total estimated pay</Cell>
-      <Cell>£4,282.20</Cell>
-    </Row>
-    <Row>
-      <Cell>Tell the mother&rsquo;s employer</Cell>
-      <Cell>28 days before they want to start maternity pay</Cell>
-    </Row>
+    <Table.Row>
+      <Table.CellHeader>First 6 weeks</Table.CellHeader>
+      <Table.Cell>£109.80 per week</Table.Cell>
+    </Table.Row>
+    <Table.Row>
+      <Table.Cell>Next 33 weeks</Table.Cell>
+      <Table.Cell>£109.80 per week</Table.Cell>
+    </Table.Row>
+    <Table.Row>
+      <Table.Cell>Total estimated pay</Table.Cell>
+      <Table.Cell>£4,282.20</Table.Cell>
+    </Table.Row>
+    <Table.Row>
+      <Table.Cell>Tell the mother&rsquo;s employer</Table.Cell>
+      <Table.Cell>28 days before they want to start maternity pay</Table.Cell>
+    </Table.Row>
   </React.Fragment>
 );
 
 const example2Head = (
-  <Row>
-    <CellHeader>Month you apply</CellHeader>
-    <CellHeader alignRight>Rate for vehicles</CellHeader>
-    <CellHeader alignRight>Rate for bicycles</CellHeader>
-  </Row>
+  <Table.Row>
+    <Table.CellHeader>Month you apply</Table.CellHeader>
+    <Table.CellHeader alignRight>Rate for vehicles</Table.CellHeader>
+    <Table.CellHeader alignRight>Rate for bicycles</Table.CellHeader>
+  </Table.Row>
 );
 
 const example2Body = (
   <React.Fragment>
-    <Row>
-      <CellHeader>January</CellHeader>
-      <Cell alignRight>£165.00</Cell>
-      <Cell alignRight>£85.00</Cell>
-    </Row>
-    <Row>
-      <CellHeader>February</CellHeader>
-      <Cell alignRight>£165.00</Cell>
-      <Cell alignRight>£85.00</Cell>
-    </Row>
-    <Row>
-      <CellHeader>March</CellHeader>
-      <Cell alignRight>£151.00</Cell>
-      <Cell alignRight>£77.00</Cell>
-    </Row>
-    <Row>
-      <CellHeader>April</CellHeader>
-      <Cell alignRight>£136.00</Cell>
-      <Cell alignRight>£70.00</Cell>
-    </Row>
+    <Table.Row>
+      <Table.CellHeader>January</Table.CellHeader>
+      <Table.Cell alignRight>£165.00</Table.Cell>
+      <Table.Cell alignRight>£85.00</Table.Cell>
+    </Table.Row>
+    <Table.Row>
+      <Table.CellHeader>February</Table.CellHeader>
+      <Table.Cell alignRight>£165.00</Table.Cell>
+      <Table.Cell alignRight>£85.00</Table.Cell>
+    </Table.Row>
+    <Table.Row>
+      <Table.CellHeader>March</Table.CellHeader>
+      <Table.Cell alignRight>£151.00</Table.Cell>
+      <Table.Cell alignRight>£77.00</Table.Cell>
+    </Table.Row>
+    <Table.Row>
+      <Table.CellHeader>April</Table.CellHeader>
+      <Table.Cell alignRight>£136.00</Table.Cell>
+      <Table.Cell alignRight>£70.00</Table.Cell>
+    </Table.Row>
   </React.Fragment>
 );
 
