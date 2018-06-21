b4f5af9820536554b4da17c2a10c2b5fa10ca604
Mark Chambers <marksy@users.noreply.github.com>
2018-06-21 20:58:57 +1200

    added fix for backlink bgcolor and updated snapshot (#340)

--- components/back-link/src/index.js
+++ components/back-link/src/index.js
@@ -26,6 +26,7 @@ const Anchor = styled('button')({
   marginTop: '15px',
   paddingLeft: '14px',
   border: 0,
+  backgroundColor: 'transparent',
   color: `${BLACK}`,
   borderBottom: `1px solid ${BLACK}`,
   textDecoration: 'none',
