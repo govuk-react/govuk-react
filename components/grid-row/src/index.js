f115ec0b451d2635d555c960d95d77be0a2f24fa
Toby Brancher <email@tobybrancher.net>
2018-06-28 13:57:27 +0100

    Update gridRow
     - removed clear left as we never float children of gridRow
     - Promote display flex to parent and adjust direction based on the MQ

--- components/grid-row/src/index.js
+++ components/grid-row/src/index.js
@@ -1,17 +1,15 @@
-// https://github.com/alphagov/govuk_frontend_toolkit/blob/master/stylesheets/_grid_layout.scss
-// https://github.com/alphagov/govuk_elements/blob/master/assets/sass/elements/_layout.scss
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 import { MEDIA_QUERIES, SPACING } from '@govuk-react/constants';
 
 const StyledRow = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
   width: '100%',
-  clear: 'left',
   margin: `0 0 ${SPACING.SCALE_3}`,
   [MEDIA_QUERIES.LARGESCREEN]: {
-    display: 'flex',
+    flexDirection: 'row',
     marginBottom: SPACING.SCALE_5,
   },
 });
