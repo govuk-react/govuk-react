308242fd21770236e45903208deee97b4498d71f
Hom Bahrani <hab0184@gmail.com>
2018-07-03 10:53:10 +0100

    update tests

--- components/tabbed-nav/src/test.js
+++ components/tabbed-nav/src/test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
-import { NavBar } from '.';
+import TabbedNav from '.';
 
 const mockHandlerFunction = jest.fn();
 
@@ -11,37 +11,37 @@ const props = {
   navItems: ['Just', 'Contact', 'Circumstances', 'Category', 'Own'],
 };
 
-describe('<NavBar />', () => {
+describe('<TabbedNav />', () => {
   it('renders without crashing', () => {
-    const NavBarWrapper = shallow(<NavBar {...props} />);
+    const TabbedNavWrapper = shallow(<TabbedNav {...props} />);
     const div = document.createElement('div');
-    ReactDOM.render(NavBarWrapper, div);
+    ReactDOM.render(TabbedNavWrapper, div);
   });
 
   it('matches the snapshot', () => {
-    const NavBarWrapper = shallow(<NavBar {...props} />);
-    expect(NavBarWrapper).toMatchSnapshot();
+    const TabbedNavWrapper = shallow(<TabbedNav {...props} />);
+    expect(TabbedNavWrapper).toMatchSnapshot();
   });
 
   it('should render the active tab by default', () => {
-    const NavBarWrapper = mount(<NavBar {...props} />);
-    expect(NavBarWrapper.find('Styled(a)').at(0).prop('active')).toEqual(true);
+    const TabbedNavWrapper = mount(<TabbedNav {...props} />);
+    expect(TabbedNavWrapper.find('Styled(a)').at(0).prop('active')).toEqual(true);
   });
 
   it('should display all the tabs passed in as props', () => {
-    const NavBarWrapper = mount(<NavBar {...props} />);
-    expect(NavBarWrapper.find('a').at(0).text()).toEqual(props.navItems[0]);
-    expect(NavBarWrapper.find('a').at(1).text()).toEqual(props.navItems[1]);
-    expect(NavBarWrapper.find('a').at(2).text()).toEqual(props.navItems[2]);
-    expect(NavBarWrapper.find('a').at(3).text()).toEqual(props.navItems[3]);
-    expect(NavBarWrapper.find('a').at(4).text()).toEqual(props.navItems[4]);
+    const TabbedNavWrapper = mount(<TabbedNav {...props} />);
+    expect(TabbedNavWrapper.find('a').at(0).text()).toEqual(props.navItems[0]);
+    expect(TabbedNavWrapper.find('a').at(1).text()).toEqual(props.navItems[1]);
+    expect(TabbedNavWrapper.find('a').at(2).text()).toEqual(props.navItems[2]);
+    expect(TabbedNavWrapper.find('a').at(3).text()).toEqual(props.navItems[3]);
+    expect(TabbedNavWrapper.find('a').at(4).text()).toEqual(props.navItems[4]);
   });
 
   describe('when a tab is clicked', () => {
-    const NavBarWrapper = mount(<NavBar {...props} />);
+    const TabbedNavWrapper = mount(<TabbedNav {...props} />);
 
     beforeEach(() => {
-      NavBarWrapper.find('a').at(1).simulate('click');
+      TabbedNavWrapper.find('a').at(1).simulate('click');
     });
 
     it('calls the function passed down as a prop when a tab is clicked', () => {
