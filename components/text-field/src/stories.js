ac58d69e449998167923e70568579865b03546df
Toby Brancher <email@tobybrancher.net>
2018-07-04 13:10:08 +0100

    More new form field element into its own section of storybook

--- components/text-field/src/stories.js
+++ components/text-field/src/stories.js
@@ -6,8 +6,8 @@ import { WithDocsCustom } from '@govuk-react/storybook-components';
 import { TextFieldFinalForm, TextFieldWithHint, TextFieldWithError, TextFieldWithKnobs } from './fixtures';
 import ReadMe from '../README.md';
 
-const stories = storiesOf('Form/TextField', module);
-const examples = storiesOf('Form/TextField/Examples', module);
+const stories = storiesOf('Form/Fields/TextField', module);
+const examples = storiesOf('Form/Fields/TextField/Examples', module);
 
 stories.addDecorator(withKnobs);
 stories.addDecorator(WithDocsCustom(ReadMe));
