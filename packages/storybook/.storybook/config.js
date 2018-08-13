35aa204610f7fbe9d1022de2a6c71ebfa0b5af14
Gavin Orland <gavin.orland@gmail.com>
2018-08-13 11:07:49 +0100

    Feature/storybook enhancements (#374)
    * Add Storybook options
    * readme rendered as top story

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -1,5 +1,11 @@
 import { configure } from '@storybook/react';
 import 'react-chromatic/storybook-addon';
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  name: 'govuk-react',
+  sortStoriesByKind: true,
+});
 
 function loadStories() {
   require('../src/stories');
