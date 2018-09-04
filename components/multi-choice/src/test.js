653d2c26adbab4da52eddc64ab8ccbd860390c66
Gavin Orland <gavin@gavinorland.com>
2018-09-04 16:22:13 +0100

    Bring MultiChoice test coverage to 100%
    Also rewrote tests to be more explicit and to no longer send in unexpected `error` prop.

--- components/multi-choice/src/test.js
+++ components/multi-choice/src/test.js
@@ -1,38 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { mount } from 'enzyme';
 
 import MultiChoice from './';
 
 describe(MultiChoice, () => {
-  const example = 'example';
-  const meta = {
-    touched: true,
-    error: example,
-  };
-  const wrapper = (
-    <MultiChoice meta={meta} hint={example} label={example}>
-      {example}
-    </MultiChoice>);
-
-  const withError = (
-    <MultiChoice label={example} error={example}>
-      {example}
-    </MultiChoice>);
-
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(
-      wrapper,
-      div,
-    );
+  it('renders with a label', () => {
+    const wrapper = mount(<MultiChoice label="Label">Example</MultiChoice>);
+    expect(wrapper.find('LabelText').contains('Label')).toBe(true);
   });
 
-  it('matches wrapper snapshot', () => {
-    expect(mount(wrapper)).toMatchSnapshot('wrapper mount');
+  it('renders with a hint', () => {
+    const wrapper = mount(<MultiChoice label="Label" hint="Hint">Example</MultiChoice>);
+    expect(wrapper.find('HintText').contains('Hint')).toBe(true);
+  });
+
+  it('matches snapshot', () => {
+    const wrapper = mount(<MultiChoice label="Label" hint="Hint">Example</MultiChoice>);
+    expect(wrapper).toMatchSnapshot('should not have ErrorText');
+  });
+
+  it('renders with error', () => {
+    const metaData = { touched: true, error: 'Example error' };
+    const wrapper = mount(<MultiChoice label="Label" meta={metaData}>Example</MultiChoice>);
+    expect(wrapper.find('ErrorText').contains('Example error')).toBe(true);
   });
 
   it('matches withError snapshot', () => {
-    expect(mount(withError)).toMatchSnapshot('with error mount');
+    const metaData = { touched: true, error: 'Example error' };
+    const wrapper = mount(<MultiChoice label="Label" meta={metaData}>Example</MultiChoice>);
+    expect(wrapper).toMatchSnapshot('should have ErrorText');
   });
 });
