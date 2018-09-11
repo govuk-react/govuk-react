b6653f4bdc5f6d53ad925a68e20e40b1806b45ad
A. P. Malde <anisha.malde@ibm.com>
2018-09-11 10:57:44 +0100

    prettier fixes

--- components/top-nav/src/atoms/right-hand-side/index.js
+++ components/top-nav/src/atoms/right-hand-side/index.js
@@ -1,17 +1,17 @@
-import styled from "react-emotion";
-import { MEDIA_QUERIES, SPACING } from "@govuk-react/constants";
+import styled from 'react-emotion';
+import { MEDIA_QUERIES, SPACING } from '@govuk-react/constants';
 
 const RightHandSide = styled("div")({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "flex-start",
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
   marginTop: SPACING.SCALE_2,
   [MEDIA_QUERIES.LARGESCREEN]: {
-    alignItems: "flex-start",
-    width: "66.66%",
+    alignItems: 'flex-start',
+    width: '66.66%',
     paddingLeft: SPACING.SCALE_3,
-    marginTop: 0
-  }
+    marginTop: 0,
+  },
 });
 
 export default RightHandSide;
