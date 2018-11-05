ded7d530754fc5ed937acd9f197897fd522678f7
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-11-05 15:03:47 +0000

    main css inline with govuk-frontend

--- components/page/src/atoms/main.js
+++ components/page/src/atoms/main.js
@@ -9,6 +9,10 @@ const Main = styled('main')({
   // tracking https://github.com/alphagov/govuk-frontend/blob/master/src/objects/_main-wrapper.scss
   paddingTop: SPACING.SCALE_4,
   paddingBottom: SPACING.SCALE_4,
+  // In IE11 the `main` element can be used, but is not recognized  –
+  // meaning it's not defined in IE's default style sheet,
+  // so it uses CSS initial value, which is inline.
+  display: 'block',
   [MEDIA_QUERIES.LARGESCREEN]: {
     paddingTop: SPACING.SCALE_5,
     paddingBottom: SPACING.SCALE_5,
