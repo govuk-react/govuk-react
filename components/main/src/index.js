ace47716b8a1adff98d291080276f982c096845b
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-11-05 14:55:41 +0000

    note around deprecating main component

--- components/main/src/index.js
+++ components/main/src/index.js
@@ -8,6 +8,8 @@ const OuterContainer = styled('div')({
   textAlign: 'center',
 });
 
+// This is currently 'width-container' and not 'main' from govuk-frontend
+// Maybe we should deprecate this component in favour of Page.Main and Page.WidthContainer?
 const InnerContainer = styled('div')({
   maxWidth: SITE_WIDTH,
   marginLeft: SPACING.SCALE_3,
