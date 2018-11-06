917b5ec300411d97cf2b3310f8f6384954f8ea9c
Gavin Orland <gavin.orland@gmail.com>
2018-11-06 14:36:14 +0000

    Link logo to govuk-react repo (#455)
    * Link logo to govuk-react repo
    
    * Fix link URL

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -5,6 +5,7 @@ import { setOptions } from '@storybook/addon-options';
 setOptions({
   name: 'govuk-react',
   sortStoriesByKind: true,
+  url: 'https://github.com/UKHomeOffice/govuk-react'
 });
 
 function loadStories() {
