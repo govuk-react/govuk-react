010dd92982fd926ac23b20f5a041d82ed07428e8
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-11-08 15:51:52 +0000

    correct name of npm script

--- scripts/chromatic.sh
+++ scripts/chromatic.sh
@@ -2,10 +2,10 @@ if [[ $TRAVIS_EVENT_TYPE != 'pull_request' || $TRAVIS_PULL_REQUEST_SLUG != $TRAV
 then
    if [ "${TRAVIS_BRANCH}" != "master" ];
    then
-     yarn chromatic;
+     yarn chromatic-test;
    else
      # http://docs.chromaticqa.com/setup_ci
      # We know any changes that make it to master *must* have been approved
-     yarn chromatic --auto-accept-changes;
+     yarn chromatic-test --auto-accept-changes;
    fi;
 fi;
