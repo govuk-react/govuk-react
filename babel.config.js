f3cac8d7a654655ae4f9a4c7fa188820a5070328
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-11-16 21:51:14 +0000

    eslint fix and babel emotion

--- babel.config.js
+++ babel.config.js
@@ -37,48 +37,49 @@
 //   ignore
 // }
 
-const esModules = (process.env["BABEL_ENV"] === "es");
+const esModules = (process.env.BABEL_ENV === 'es');
 
 const presets = [
-  "@babel/preset-env",
-  "@babel/preset-react"
+  '@babel/preset-env',
+  '@babel/preset-react',
 ];
 
-const plugins =  [
-  "@babel/plugin-proposal-object-rest-spread",
-  "@babel/plugin-proposal-class-properties",
-  // "@babel/plugin-proposal-decorators",
-  "@babel/plugin-proposal-do-expressions",
-  "@babel/plugin-proposal-export-default-from",
-  "@babel/plugin-proposal-export-namespace-from",
-  "@babel/plugin-proposal-function-sent",
-  "@babel/plugin-proposal-json-strings",
-  "@babel/plugin-proposal-logical-assignment-operators",
-  "@babel/plugin-proposal-nullish-coalescing-operator",
-  "@babel/plugin-proposal-numeric-separator",
-  "@babel/plugin-proposal-optional-chaining",
-  // "@babel/plugin-proposal-pipeline-operator",
+const plugins = [
+  '@babel/plugin-proposal-object-rest-spread',
+  '@babel/plugin-proposal-class-properties',
+  // '@babel/plugin-proposal-decorators',
+  '@babel/plugin-proposal-do-expressions',
+  '@babel/plugin-proposal-export-default-from',
+  '@babel/plugin-proposal-export-namespace-from',
+  '@babel/plugin-proposal-function-sent',
+  '@babel/plugin-proposal-json-strings',
+  '@babel/plugin-proposal-logical-assignment-operators',
+  '@babel/plugin-proposal-nullish-coalescing-operator',
+  '@babel/plugin-proposal-numeric-separator',
+  '@babel/plugin-proposal-optional-chaining',
+  // '@babel/plugin-proposal-pipeline-operator',
   '@babel/plugin-proposal-throw-expressions',
-  "@babel/plugin-syntax-dynamic-import",
-  "@babel/plugin-syntax-import-meta",
+  '@babel/plugin-syntax-dynamic-import',
+  '@babel/plugin-syntax-import-meta',
   [
-    "@babel/plugin-transform-runtime",
-    { "helpers": false, /*"polyfill": false,*/ "regenerator": true }
-  ]
+    '@babel/plugin-transform-runtime',
+    { helpers: false, /* polyfill: false, */ regenerator: true },
+  ],
+  ['emotion', { autoLabel: true }],
 ];
 
-const ignore = [".spec.js", ".test.js", "-test.js", "/__tests__/"];
+const ignore = ['.spec.js', '.test.js', '-test.js', '/__tests__/'];
 
 if (!esModules) {
-  presets[0] = ["@babel/preset-env", {
-    "loose": true,
-    "modules": "commonjs"
+  presets[0] = ['@babel/preset-env', {
+    loose: true,
+    modules: 'commonjs',
   }];
-  plugins.push("add-module-exports")
+  plugins.push('add-module-exports');
 } else {
-  presets[0] = ["@babel/preset-env", {
-    "loose": true,
-    "modules": false
+  presets[0] = ['@babel/preset-env', {
+    loose: true,
+    modules: false,
   }];
 }
 
