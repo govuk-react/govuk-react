9b42259f8db13f8c1403a415d01f8a6f53795725
Alasdair McLeay <penx@users.noreply.github.com>
2018-11-19 16:24:34 +0000

    Upgrade to babel 7 (#467)
    * jest, enzyme, emotion upgrade
    
    * refresh lock and update snapshots
    
    * update yarn lock
    
    * rename babelrc to babel config
    
    * upgrade lerna and babel deps
    
    * storybook babelrc
    
    * babel updates in api-docs
    
    * use root babel config
    
    * use yarn for build scripts and upgrade storybook
    
    * testurl in jest config to work around enzyme issue
    
    * use yarn for all scripts, correct testURL
    
    * don't use babel preset, core bridge now at root
    
    * yarn lock
    
    * eslint fix and babel emotion
    
    * yarn lock
    
    * babel-loader is in devDepencies already due to rebase
    
    * copy stage-1 directly from babel docs
    
    * restore transform-react-remove-prop-types and babel/plugin-transform-runtime
    
    * babel config minor
    
    * yarn lock
    
    * update snapshots from fresh clone
    
    * update react-chromatic

---
+++ babel.config.js
@@ -0,0 +1,63 @@
+const esModules = (process.env.BABEL_ENV === 'es');
+
+const presets = [
+  '@babel/preset-env',
+  '@babel/preset-react',
+];
+
+const plugins = [
+  // Stage 1
+  '@babel/plugin-proposal-export-default-from',
+  '@babel/plugin-proposal-logical-assignment-operators',
+  ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+  ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+  ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
+  '@babel/plugin-proposal-do-expressions',
+
+  // Stage 2
+  ['@babel/plugin-proposal-decorators', { legacy: true }],
+  '@babel/plugin-proposal-function-sent',
+  '@babel/plugin-proposal-export-namespace-from',
+  '@babel/plugin-proposal-numeric-separator',
+  '@babel/plugin-proposal-throw-expressions',
+
+  // Stage 3
+  '@babel/plugin-syntax-dynamic-import',
+  '@babel/plugin-syntax-import-meta',
+  ['@babel/plugin-proposal-class-properties', { loose: false }],
+  '@babel/plugin-proposal-json-strings',
+
+
+  '@babel/plugin-proposal-object-rest-spread',
+
+  ['transform-react-remove-prop-types'],
+  [
+    '@babel/plugin-transform-runtime',
+    { helpers: false, regenerator: true },
+  ],
+  ['emotion', { autoLabel: true }],
+];
+
+const ignore = ['.spec.js', '.test.js', '-test.js', '/__tests__/'];
+
+if (!esModules) {
+  presets[0] = ['@babel/preset-env', {
+    loose: true,
+    modules: 'commonjs',
+  }];
+  plugins.push('add-module-exports');
+} else {
+  presets[0] = ['@babel/preset-env', {
+    loose: true,
+    modules: false,
+  }];
+}
+
+module.exports = function (api) {
+  const env = api.cache(() => process.env.NODE_ENV);
+  return {
+    presets,
+    plugins,
+    ignore,
+  };
+};
