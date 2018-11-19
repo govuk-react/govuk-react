e4b397c2cfd734ad4b01ec8a3db2bbc02e662ff0
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-11-19 11:25:31 +0000

    copy stage-1 directly from babel docs

--- babel.config.js
+++ babel.config.js
@@ -45,26 +45,26 @@ const presets = [
 ];
 
 const plugins = [
-  '@babel/plugin-proposal-object-rest-spread',
-  '@babel/plugin-proposal-class-properties',
-  // '@babel/plugin-proposal-decorators',
-  '@babel/plugin-proposal-do-expressions',
+  // Stage 1
   '@babel/plugin-proposal-export-default-from',
-  '@babel/plugin-proposal-export-namespace-from',
-  '@babel/plugin-proposal-function-sent',
-  '@babel/plugin-proposal-json-strings',
   '@babel/plugin-proposal-logical-assignment-operators',
-  '@babel/plugin-proposal-nullish-coalescing-operator',
+  ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+  ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+  ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
+  '@babel/plugin-proposal-do-expressions',
+
+  // Stage 2
+  ['@babel/plugin-proposal-decorators', { legacy: true }],
+  '@babel/plugin-proposal-function-sent',
+  '@babel/plugin-proposal-export-namespace-from',
   '@babel/plugin-proposal-numeric-separator',
-  '@babel/plugin-proposal-optional-chaining',
-  // '@babel/plugin-proposal-pipeline-operator',
   '@babel/plugin-proposal-throw-expressions',
+
+  // Stage 3
   '@babel/plugin-syntax-dynamic-import',
   '@babel/plugin-syntax-import-meta',
-  [
-    '@babel/plugin-transform-runtime',
-    { helpers: false, /* polyfill: false, */ regenerator: true },
-  ],
+  ['@babel/plugin-proposal-class-properties', { loose: false }],
+  '@babel/plugin-proposal-json-strings',
   ['emotion', { autoLabel: true }],
 ];
 
