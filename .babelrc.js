c69a046a3e5b49cf8642776ebdfed310a244b6a6
Alasdair McLeay <penx@users.noreply.github.com>
2018-11-19 12:17:35 +0000

    babel presets have been deprecated, replace (#471)

--- .babelrc.js
+++ .babelrc.js
@@ -2,11 +2,34 @@ const esModules = (process.env["BABEL_ENV"] === "es");
 
 const presets = [
   "@babel/preset-env",
-  "@babel/preset-react",
-  "@babel/preset-stage-1"
+  "@babel/preset-react"
 ];
 
 const plugins =  [
+  // Stage 1
+  "@babel/plugin-proposal-export-default-from",
+  "@babel/plugin-proposal-logical-assignment-operators",
+  ["@babel/plugin-proposal-optional-chaining", { loose: false }],
+  ["@babel/plugin-proposal-pipeline-operator", { proposal: "minimal" }],
+  ["@babel/plugin-proposal-nullish-coalescing-operator", { loose: false }],
+  "@babel/plugin-proposal-do-expressions",
+
+  // Stage 2
+  ["@babel/plugin-proposal-decorators", { legacy: true }],
+  "@babel/plugin-proposal-function-sent",
+  "@babel/plugin-proposal-export-namespace-from",
+  "@babel/plugin-proposal-numeric-separator",
+  "@babel/plugin-proposal-throw-expressions",
+
+  // Stage 3
+  "@babel/plugin-syntax-dynamic-import",
+  "@babel/plugin-syntax-import-meta",
+  ["@babel/plugin-proposal-class-properties", { loose: false }],
+  "@babel/plugin-proposal-json-strings",
+
+
+  "@babel/plugin-proposal-object-rest-spread",
+
   "transform-decorators-legacy",
   ["transform-react-remove-prop-types"],
   [
