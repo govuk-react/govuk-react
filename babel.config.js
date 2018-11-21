80b172066c3721dd79879f4d43532544b83a4854
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-11-21 08:30:46 +0000

    disable stage 1 that aren't needed for build

--- babel.config.js
+++ babel.config.js
@@ -8,11 +8,11 @@ const presets = [
 const plugins = [
   // Stage 1
   '@babel/plugin-proposal-export-default-from',
-  '@babel/plugin-proposal-logical-assignment-operators',
-  ['@babel/plugin-proposal-optional-chaining', { loose: false }],
-  ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-  ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
-  '@babel/plugin-proposal-do-expressions',
+  // '@babel/plugin-proposal-logical-assignment-operators',
+  // ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+  // ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+  // ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
+  // '@babel/plugin-proposal-do-expressions',
 
   // Stage 2
   ['@babel/plugin-proposal-decorators', { legacy: true }],
