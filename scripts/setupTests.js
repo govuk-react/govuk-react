4ac49f441012f570159c9a7b4f81f55b05d50b38
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-01-10 16:29:33 +0000

    emotion 10 serializer for tests

--- scripts/setupTests.js
+++ scripts/setupTests.js
@@ -1,8 +1,8 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { createSerializer } from 'jest-emotion';
-import * as emotion from 'emotion';
+import styled from '@emotion/styled'
 
-expect.addSnapshotSerializer(createSerializer(emotion));
+expect.addSnapshotSerializer(createSerializer(styled));
 
 configure({ adapter: new Adapter() });
