3d471319282a46f1b7265943e62a22fa30ea1532
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-01-10 16:30:25 +0000

    use mount rather than shallow in withWhiteSpace tests (and fix non independent tests)

--- packages/hoc/src/withWhiteSpace/test.js
+++ packages/hoc/src/withWhiteSpace/test.js
@@ -1,30 +1,31 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import InputField from '@govuk-react/input-field';
 import withWhiteSpace from './';
 
 const WithoutConfig = withWhiteSpace()(InputField);
 const WithConfig = withWhiteSpace({ marginBottom: 0 })(InputField);
-let wrapper;
 
 describe('withWhiteSpace', () => {
   it('renders without config without crashing', () => {
-    shallow(<WithoutConfig>Example</WithoutConfig>);
+    mount(<WithoutConfig>Example</WithoutConfig>);
   });
 
   it('renders with config without crashing', () => {
-    shallow(<WithConfig>Example</WithConfig>);
+    mount(<WithConfig>Example</WithConfig>);
   });
 
   it('renders with props without crashing', () => {
-    wrapper = shallow(<WithConfig mb={5}>Example</WithConfig>);
+    mount(<WithConfig mb={5}>Example</WithConfig>);
   });
 
   it('renders an InputField', () => {
+    const wrapper = mount(<WithConfig mb={5}>Example</WithConfig>);
     expect(wrapper.find('InputField').exists()).toBe(true);
   });
 
   it('matches wrapper snapshot', () => {
+    const wrapper = mount(<WithConfig mb={5}>Example</WithConfig>);
     expect(wrapper).toMatchSnapshot();
   });
 });
