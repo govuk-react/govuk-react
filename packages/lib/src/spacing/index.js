771b656062a3425993d74ac6417269bfa560198a
Steve Sims <steve.sims@ibm.com>
2019-01-31 17:24:08 +0000

    Add `withWhiteSpace` to spacing lib
    * adds new `withWhiteSpace` function to spacing lib
      - generates spacing styles, accepting `margin`, `padding` and `mb` props
      - intended for use within styled component definitions
    
    `withWhiteSpace` HOC refactored to use function, but is now deprecated

--- packages/lib/src/spacing/index.js
+++ packages/lib/src/spacing/index.js
@@ -1,10 +1,15 @@
 // This lib is effectively a port of govuk-frontend's spacing sass helpers
 // Tracking:
 // https://github.com/alphagov/govuk-frontend/blob/master/src/helpers/_spacing.scss
+// Further references:
+// https://github.com/alphagov/govuk-frontend/blob/master/src/overrides/_spacing.scss
+// https://github.com/alphagov/govuk-frontend/blob/master/src/settings/_spacing.scss
 
+import PropTypes from 'prop-types';
 import {
   MEDIA_QUERIES,
   SPACING_MAP,
+  SPACING_MAP_INDEX,
   SPACING_POINTS,
 } from '@govuk-react/constants';
 
@@ -87,3 +92,70 @@ export function responsivePadding(value) {
     size, property: 'padding', direction, adjustment,
   });
 }
+
+// withWhiteSpace lib styling function
+// does not form part of govuk-frontend
+
+// `withWhiteSpace(config)`
+// generates a styling function, based on config, which can be used when styling a component
+
+// `margin` and `padding` are supported as props and config values
+// can be a single number, to indicate scale size to use in all directions
+// can be an object in format `{ size, direction, adjustment }`
+// - see `responsivePadding` and `responsiveMargin` calls
+// can be an array of numbers/objects
+
+export function withWhiteSpace(config = {}) {
+  return ({
+    margin = config.margin,
+    padding = config.padding,
+    mb: marginBottom = config.marginBottom,
+  } = {}) => {
+    const styles = [];
+
+    if (margin !== undefined) {
+      if (Array.isArray(margin)) {
+        styles.push(margin.map(val => responsiveMargin(val)));
+      } else {
+        styles.push(responsiveMargin(margin));
+      }
+    }
+
+    if (padding !== undefined) {
+      if (Array.isArray(padding)) {
+        styles.push(padding.map(val => responsivePadding(val)));
+      } else {
+        styles.push(responsivePadding(padding));
+      }
+    }
+
+    if (marginBottom !== undefined) {
+      styles.push(responsiveMargin({ size: marginBottom, direction: 'bottom' }));
+    }
+
+    return styles;
+  };
+}
+
+const Directions = PropTypes.oneOf(['all', 'top', 'right', 'bottom', 'left']);
+
+const SpacingShape = PropTypes.shape({
+  size: PropTypes.number.isRequired,
+  direction: PropTypes.oneOfType([Directions, PropTypes.arrayOf(Directions)]),
+  adjustment: PropTypes.number,
+});
+
+// `mb` (Margin Bottom) prop name comes from the naming convention used by https://github.com/jxnblk/grid-styled
+withWhiteSpace.propTypes = {
+  mb: PropTypes.oneOf(SPACING_MAP_INDEX),
+  margin: PropTypes.oneOfType([
+    PropTypes.number,
+    SpacingShape,
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.number, SpacingShape])),
+  ]),
+  padding: PropTypes.oneOfType([
+    PropTypes.number,
+    SpacingShape,
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.number, SpacingShape])),
+  ]),
+};
