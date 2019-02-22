beee710157c56a6ba38696b095aaf638cc3db1d2
David <david-murdoch-wsps@NLEDP.HOMEOFFICE.GOV.UK>
2019-02-22 16:49:17 +0000

    Changed RegExp in webpack.config.js
    A RegExp in webpack.config.js has been changed to implement nodejs' path.sep that results in a different directory separator on different machines unix (forward slash) vs. windows (backslash). This allows storybook to render on windows machines. This problem was also preventing successful builds since @govuk-react/storybook is part of the build process.
    
    Credit where credit is due. Steve fixed this...

--- packages/storybook/.storybook/webpack.config.js
+++ packages/storybook/.storybook/webpack.config.js
@@ -1,30 +1,34 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-const aliasToSrc = require('./aliasToSrc');
+const aliasToSrc = require("./aliasToSrc");
 
 // https://stackoverflow.com/questions/18112204/get-all-directories-within-directory-nodejs?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-const dirs = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory()).map(f => path.join(p, f));
+const dirs = p =>
+  fs
+    .readdirSync(p)
+    .filter(f => fs.statSync(path.join(p, f)).isDirectory())
+    .map(f => path.join(p, f));
 
 module.exports = (baseConfig, env, defaultConfig) => {
-
   // Include PNGs imported from node_modules
   defaultConfig.module.rules.push({
     test: /\.png/,
     loaders: ["url-loader"],
-    include: path.resolve(__dirname, '../../..')
+    include: path.resolve(__dirname, "../../..")
   });
   // make babel-loader process components and packages src folder
-  defaultConfig.module.rules[0].include.push(/\/(packages|components)\/[^/]+\/src/);
+  const pathRegex = new RegExp(`\\${path.sep}(packages|components)\\${path.sep}[^\\${path.sep}]+\\${path.sep}src`);
+  defaultConfig.module.rules[0].include.push(pathRegex);
   // use babel.config.js from the monorepo root by telling babel to search upward beyond storybook package.json
   defaultConfig.module.rules[0].use[0].options.rootMode = "upward";
 
   // when one component depends on another, we want to webpack to load from the component's src folder so that it auto refreshes when there are changes
   defaultConfig.resolve.alias = {
     ...defaultConfig.resolve.alias,
-    ...aliasToSrc(dirs('../../components')),
-    ...aliasToSrc(dirs('../../packages'))
-  }
+    ...aliasToSrc(dirs("../../components")),
+    ...aliasToSrc(dirs("../../packages"))
+  };
 
   return defaultConfig;
 };
