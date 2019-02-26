9fbd298d38ee5af600892806ced92f1077976c38
Steve Sims <steve.sims@ibm.com>
2019-02-26 14:15:31 +0000

    Deprecate the things
    `Header` and `SupportingHeader` components and `withWhiteSpace` HOC now deprecated

--- packages/hoc/src/withWhiteSpace/index.js
+++ packages/hoc/src/withWhiteSpace/index.js
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
 import { spacing } from '@govuk-react/lib';
 
+import deprecated from '../deprecated';
+
 // NB withWhiteSpace HOC is DEPRECATED
 // Please use `spacing.withWhiteSpace(config)` instead in styled components
 
 const withWhiteSpace = config => (Component) => {
-  const StyledHoc = styled(Component)(spacing.withWhiteSpace(config));
+  const StyledHoc = styled(deprecated(
+    Component,
+    '(use of withWhiteSpace HOC - Please use `spacing.withWhiteSpace(config)` instead in styled components)',
+  ))(spacing.withWhiteSpace(config));
 
   StyledHoc.propTypes = {
     ...spacing.withWhiteSpace.propTypes,
