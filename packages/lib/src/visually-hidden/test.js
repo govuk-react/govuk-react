c34ceefc2c270eae3d41d01b61e7c4de8e4f6aac
Steve Sims <steve_sims7@yahoo.co.uk>
2019-03-08 11:06:46 +0000

    Add VisuallyHidden component and lib (#588)
    * Add VisuallyHidden component and lib
    
    adds new `VisuallyHidden` component, which styles a span as visually-hidden replicating the most common use of that class within govuk-frontend
    
    adds `visuallyHidden()` and `visuallyHidden.focusable()` to `@govuk-react/lib`, tracking equivalent sass helper in govuk-frontend
    
    * Add new example stories to VisuallyHidden
    
    Also wrap `Button` component in `React.forwardRef` to allow it to be auto-focusable
    
    * Attempt to fix chromatic capture of focusable

---
+++ packages/lib/src/visually-hidden/test.js
@@ -0,0 +1,70 @@
+import visuallyHidden, { focusable } from '.';
+
+describe('visuallyHidden lib', () => {
+  describe('visuallyHidden', () => {
+    it('produces a style declaration', () => {
+      const style = visuallyHidden();
+
+      expect(style).toBeTruthy();
+      expect(style).toBeInstanceOf(Object);
+    });
+
+    it('produces styles which, by default, are marked as important', () => {
+      const style = visuallyHidden();
+
+      Object.values(style).forEach((value) => {
+        expect(value).toContain(' !important');
+      });
+    });
+
+    it('can produce styles that do not contain important', () => {
+      const style = visuallyHidden({ important: false });
+
+      Object.values(style).forEach((value) => {
+        expect(value).not.toContain('!important');
+      });
+    });
+  });
+
+  describe('visuallyHidden.focusable', () => {
+    it('produces a style declaration, with focus and active styles', () => {
+      const style = focusable();
+
+      expect(style).toBeTruthy();
+      expect(style).toBeInstanceOf(Object);
+
+      const hasStyles = Object.keys(style).some(key =>
+        [':focus', ':active'].some(checkKey => key.includes(checkKey)));
+
+      expect(hasStyles).toBe(true);
+    });
+
+    it('produces styles which, by default, are marked as important', () => {
+      const style = visuallyHidden();
+
+      Object.entries(style).forEach(([key, value]) => {
+        if ([':focus', ':active'].some(checkKey => key.includes(checkKey))) {
+          Object.values(value).forEach((activeValue) => {
+            expect(activeValue).toContain(' !important');
+          });
+        } else {
+          expect(value).toContain(' !important');
+        }
+      });
+    });
+
+    it('can produce styles that do not contain important', () => {
+      const style = visuallyHidden({ important: false });
+
+      Object.entries(style).forEach(([key, value]) => {
+        if ([':focus', ':active'].some(checkKey => key.includes(checkKey))) {
+          Object.values(value).forEach((activeValue) => {
+            expect(activeValue).not.toContain(' !important');
+          });
+        } else {
+          expect(value).not.toContain(' !important');
+        }
+      });
+    });
+  });
+});
