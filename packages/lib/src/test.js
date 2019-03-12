da282d2f7b5b7b086d25895c6708537f94d9b4b2
Steve Sims <steve.sims@ibm.com>
2019-03-12 12:34:53 +0000

    Add basic export test for @govuk-react/lib

---
+++ packages/lib/src/test.js
@@ -0,0 +1,8 @@
+import * as packageExports from '.';
+
+describe('Exports', () => {
+  // all exports are truthy
+  Object.keys(packageExports).map(exportName => it(`${exportName} is truthy`, () => {
+    expect(!!packageExports[exportName]).toBe(true);
+  }));
+});
