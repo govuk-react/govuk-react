1c1d8e98b223b09fb356cfa1d107b19b565b12c5
David <david-murdoch-wsps@NLEDP.HOMEOFFICE.GOV.UK>
2019-03-21 11:40:44 +0000

    all comments in jest.config.js inside module.exports have been removed

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,8 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-  // Automatically clear mock calls and instances between every test
   clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
     '<rootDir>/{components,packages}/*/src/**.[jt]s?(x)',
     '<rootDir>/{components,packages}/*/src/**/*.[jt]s?(x)',
@@ -17,11 +12,7 @@ module.exports = {
     '!<rootDir>/packages/api-docs/**',
     '!<rootDir>/packages/storybook-components/**',
   ],
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: './coverage/',
-
-  // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
       branches: 95,
@@ -30,8 +21,6 @@ module.exports = {
       statements: 95,
     },
   },
-
-  // An array of file extensions your modules use
   moduleFileExtensions: [
     'web.js',
     'js',
@@ -40,29 +29,17 @@ module.exports = {
     'jsx',
     'node',
   ],
-
-  // The path to a module that runs some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ['<rootDir>/scripts/setupTests.js'],
-
-  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   snapshotSerializers: [
     'enzyme-to-json/serializer',
   ],
-
-  // The test environment that will be used for testing
   testEnvironment: 'jsdom',
-
-  // The glob patterns Jest uses to detect test files
   testMatch: [
     '**/{components,packages}/*/src/**/?(*.)(spec|test).js?(x)',
   ],
-
-  // A map from regular expressions to paths to transformers
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
     '[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$',
   ],
