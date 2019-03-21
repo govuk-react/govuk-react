be33d8c236a018dbc5538e42a6dce1c3f02601c1
David <david-murdoch-wsps@NLEDP.HOMEOFFICE.GOV.UK>
2019-03-21 11:35:42 +0000

    Changes to jest.config.js
    Added comments from jest --init that describe the role of each configurable option along with the link to the jest docs that describes all configuration options and their defaults. Also added clearMocks and removed pointless env=jsdom statements in package.json

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
 module.exports = {
-  coverageDirectory: './coverage/',
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
     '<rootDir>/{components,packages}/*/src/**.[jt]s?(x)',
     '<rootDir>/{components,packages}/*/src/**/*.[jt]s?(x)',
@@ -9,6 +17,11 @@ module.exports = {
     '!<rootDir>/packages/api-docs/**',
     '!<rootDir>/packages/storybook-components/**',
   ],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: './coverage/',
+
+  // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
       branches: 95,
@@ -17,18 +30,8 @@ module.exports = {
       statements: 95,
     },
   },
-  setupFilesAfterEnv: ['<rootDir>/scripts/setupTests.js'],
-  testMatch: [
-    '**/{components,packages}/*/src/**/?(*.)(spec|test).js?(x)',
-  ],
-  testEnvironment: 'node',
-  testURL: 'http://localhost',
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$',
-  ],
+
+  // An array of file extensions your modules use
   moduleFileExtensions: [
     'web.js',
     'js',
@@ -37,7 +40,30 @@ module.exports = {
     'jsx',
     'node',
   ],
+
+  // The path to a module that runs some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: ['<rootDir>/scripts/setupTests.js'],
+
+  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   snapshotSerializers: [
     'enzyme-to-json/serializer',
+  ],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    '**/{components,packages}/*/src/**/?(*.)(spec|test).js?(x)',
+  ],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$',
   ],
 };
