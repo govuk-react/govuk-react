4b17862e28bf6a49798c89a2cd47a511a0749e30
Steve Sims <steve_sims7@yahoo.co.uk>
2019-03-22 15:41:04 +0000

    Storybook tweaks and fixes (#637)
    * storybook fixes
    
    Restore custom preview display to storybook, which will allow for responsive items that transform to full-width on mobile to display correctly
    * Split out StoryPreview component into a separate file
    * `withDocsCustom` has its custom component removed (as it's no longer supported in that way) and changed how `withDocs` is called to prevent deprecation warnings
    * storybook config updated for version 5 updates, and setting custom readme preview
    * Update how base/root readme works
    * removed `ReadMeHidePreview`

--- packages/storybook/src/stories/index.js
+++ packages/storybook/src/stories/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { ReadMeHidePreview } from '@govuk-react/storybook-components';
+import { addReadme } from 'storybook-readme';
 import 'normalize.css';
 import './styles.css';
 import ReadMe from '../../../../README.md';
 
 if (!navigator.userAgent.match(/Chromatic/)) {
   const stories = storiesOf(' Welcome', module);
-  stories.addDecorator(ReadMeHidePreview(ReadMe));
+  stories.addDecorator(addReadme);
+  stories.addParameters({
+    readme: {
+      content: ReadMe,
+      StoryPreview: () => null,
+    },
+  });
   stories.add('to govuk-react', () => <div />);
 }
 
