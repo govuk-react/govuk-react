5a6304a1cf6bc004cf243a57ca75d222fe706be6
Alasdair McLeay <penx@users.noreply.github.com>
2021-05-15 17:05:33 +0100

    Remove type=number from date field (#779)
    Fixes #481

--- components/date-field/src/input.js
+++ components/date-field/src/input.js
@@ -32,7 +32,6 @@ class Input extends React.Component {
         <StyledInput
           name={name}
           error={error}
-          type="number"
           defaultValue={defaultValue}
           value={this.props.value ? this.props.value[key] : undefined}
           onChange={e => this.props.onChange(e, key)}
