5f716bc8395a46a1b81a32cb878eff053f770df6
Alasdair McLeay <alasdair.mcleay@me.com>
2021-05-16 20:43:09 +0100

    Fix release script
    Fixes #447
    Fixes #669

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # requires https://hub.github.com
+git remote get-url origin | grep -q 'govuk-react/govuk-react' || exit 1 # ensure origin doesn't point to a fork
 git checkout -b release/next origin/master # make a new branch for next version from master
 git fetch --tags # lerna uses tags to determine what has changed, we need to make sure we have all tags locally
-lerna publish --skip-npm # update version numbers
+yarn lerna publish --skip-npm # update version numbers
 VERSION=$(node -e 'console.log(require("./lerna.json").version)') # get new version number
 git tag -d v$VERSION # delete local tag
 git branch -m release/v$VERSION # rename branch
