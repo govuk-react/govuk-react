3ec231f76e19bcdb3c5df086ea6732ea51f813ac
Alasdair McLeay <alasdair.mcleay@me.com>
2021-05-17 23:25:33 +0100

    move storybook config to new format

---
+++ packages/storybook/.storybook/manager.js
@@ -0,0 +1,17 @@
+import { configureReadme } from 'storybook-readme';
+import { StoryPreview } from '@govuk-react/storybook-components';
+
+import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming';
+
+const theme = create({
+  brandTitle: 'govuk-react',
+  sortStoriesByKind: true,
+  brandUrl: 'https://github.com/govuk-react/govuk-react',
+});
+
+addons.setConfig({
+  theme,
+});
+
+configureReadme({ StoryPreview });
---
+++ packages/storybook/.storybook/preview.js
@@ -0,0 +1,5 @@
+import '../src/stories'
+
+export const parameters = {
+    chromatic: { viewports: [320, 1200] },
+};
