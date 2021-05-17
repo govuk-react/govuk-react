215114b129c16236bd804dce01795b2eef856c33
Alasdair McLeay <alasdair.mcleay@me.com>
2021-05-17 23:28:35 +0100

    add storybook/addon-docs

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -2,7 +2,7 @@ const { webpack: lernaAliases } = require('lerna-alias');
 
 module.exports = {
   stories: ['../../../@(packages|components)/!(icons)/**/src/**/stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs', '@storybook/addon-docs'],
   webpackFinal: async config => {
     config.resolve.alias = lernaAliases();
     return config;
