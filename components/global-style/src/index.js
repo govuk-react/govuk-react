c24c67d926ffc827b5778c8928400c00bd7eda45
Alasdair McLeay <penx@users.noreply.github.com>
2021-05-22 11:49:20 +0100

    GlobalStyle component (#834)
    * GlobalStyle component
    
    * Fix eslint issues

---
+++ components/global-style/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Inter UI', Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+/**
+ *
+ * A Styled Component to apply global style for use with govuk-react.
+ *
+ * ### Usage
+ *
+ * Simple
+ * ```jsx
+ * import GlobalStyle from '@govuk-react/global-style';
+ *
+ * const MyApp = () => <>
+ *   <GlobalStyle />
+ *   <div>Example</div>
+ * </>
+ * ```
+ *
+ * ### References:
+ * - https://styled-components.com/docs/api#createglobalstyle
+ *
+ */
+const DocumentedGlobalStyle = (props) => <DocumentedGlobalStyle {...props} />;
+
+export default GlobalStyle;
--- packages/example-application/src/index.js
+++ packages/example-application/src/index.js
@@ -2,23 +2,12 @@ import React from 'react';
 import * as GovUK from 'govuk-react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router';
 
-import { createGlobalStyle } from 'styled-components';
 import Home from './home';
 import Form from './form';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    padding: 0;
-    margin: 0;
-    font-family: 'Inter UI', Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-`;
-
 const ExampleApplication = () => (
   <>
-    <GlobalStyle />
+    <GovUK.GlobalStyle />
     <GovUK.TopNav />
     <GovUK.Page.WidthContainer
       beforeChildren={<GovUK.PhaseBanner level="alpha">This part of GOV.UK is being rebuilt</GovUK.PhaseBanner>}
