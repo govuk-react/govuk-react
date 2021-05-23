9ed6962d1670b8fb5798e103a76881afb4ac44d1
Alasdair McLeay <alasdair.mcleay@me.com>
2021-05-23 22:43:33 +0100

    lerna publish --skip-npm => npm version --worspaces

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@
 git remote get-url origin | grep -q 'govuk-react/govuk-react' || exit 1 # ensure origin doesn't point to a fork
 git checkout -b release/next origin/main # make a new branch for next version from main
 git fetch --tags # lerna uses tags to determine what has changed, we need to make sure we have all tags locally
-yarn lerna publish --skip-npm # update version numbers
+npm version --workspaces # update version numbers
 VERSION=$(node -e 'console.log(require("./packages/govuk-react/package.json").version)') # get new version number
 git tag -d v$VERSION # delete local tag
 git branch -m release/v$VERSION # rename branch
