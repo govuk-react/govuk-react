27520b7768c472ee5cf088772f83b68efb658dcb
Alasdair McLeay <alasdair.mcleay@me.com>
2021-05-24 23:56:47 +0100

    Details

--- components/details/src/index.js
+++ components/details/src/index.js
@@ -84,6 +84,7 @@ const DetailsText = styled('div')({
  *
  *
  * Simple
+ *
  * ```jsx
  * <Details summary="Help with nationality">
  *   I am a paragraph of hidden details, to be revealed when summary is clicked
