b57c083d0a000491e02b81818da6b800934310f8
Alasdair McLeay <alasdair.mcleay@me.com>
2021-05-24 23:15:45 +0100

    Breadcrumbs

--- components/breadcrumbs/src/stories.js
+++ components/breadcrumbs/src/stories.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withDocsCustom } from '@govuk-react/storybook-components';
 
 import Breadcrumbs from '.';
-import ReadMe from '../README.md';
 
-const stories = storiesOf('Navigation/Breadcrumbs', module);
+export default {
+  title: 'Core/Breadcrumbs',
+  component: Breadcrumbs,
+};
 
-stories.addDecorator(withDocsCustom(ReadMe));
-
-stories.add('Component default', () => (
+export const Default = () => (
   <Breadcrumbs>
     <Breadcrumbs.Link href="/section">Section</Breadcrumbs.Link>
     <Breadcrumbs.Link href="/section/sub-section">Sub-section</Breadcrumbs.Link>
     Current page
   </Breadcrumbs>
-));
+);
