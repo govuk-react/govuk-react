bcb43bbcea62da38350b9f4e3b0a62ed2178f0e1
Alasdair McLeay <alasdair.mcleay@me.com>
2021-05-26 00:23:31 +0100

    Panel

--- components/panel/src/stories.js
+++ components/panel/src/stories.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withDocsCustom } from '@govuk-react/storybook-components';
 
 import Panel from '.';
-import ReadMe from '../README.md';
 
-const stories = storiesOf('Misc/Panel', module);
-const examples = storiesOf('Misc/Panel/Examples', module);
+export default {
+  title: 'Core/Panel',
+  component: Panel,
+};
 
-stories.addDecorator(withDocsCustom(ReadMe));
+export const Default = () => <Panel title="Application complete" />;
 
-stories.add('Component default', () => <Panel title="Application complete" />);
-
-examples.add('Panel with header and HTML body', () => (
+export const PanelWithHeaderAndHtmlBody = () => (
   <Panel title="Application complete">
     Your reference number
     <br />
     <strong>HDJ2123F</strong>
   </Panel>
-));
+);
