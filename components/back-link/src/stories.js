b94abd45ea3e245e792dc939d9d6e906c23dd94f
Alasdair McLeay <alasdair.mcleay@me.com>
2021-05-25 22:02:17 +0100

    BackLink formatting

--- components/back-link/src/stories.js
+++ components/back-link/src/stories.js
@@ -4,12 +4,11 @@ import { withKnobs, text } from '@storybook/addon-knobs';
 
 import BackLink from '.';
 
-export const Default = () => <BackLink onClick={action('go-back')}>{text('Children', 'Back')}</BackLink>;
-
-export const WithHref = () => <BackLink href="#">Back</BackLink>;
-
 export default {
   title: 'Core/Back link',
   component: BackLink,
   decorators: [withKnobs],
 };
+
+export const Default = () => <BackLink onClick={action('go-back')}>{text('Children', 'Back')}</BackLink>;
+export const WithHref = () => <BackLink href="#">Back</BackLink>;
