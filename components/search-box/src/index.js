a72761372730e00343617bb6dcb545b138491678
Alasdair McLeay <alasdair.mcleay@me.com>
2021-05-26 21:13:29 +0100

    SearchBox

--- components/search-box/src/index.js
+++ components/search-box/src/index.js
@@ -79,24 +79,18 @@ const StyledSearchBox = styled('div')(
  * ### Usage
  *
  * Simple
- * ```jsx
- * import Layout from '@govuk-react/layout';
- * import GridRow from '@govuk-react/grid-row';
- * import GridCol from '@govuk-react/grid-col';
  *
- * <Layout>
- *    <GridRow>
- *      <GridCol>
- *        <SearchBox>
- *          <SearchBox.Input placeholder="Search GOV.UK" />
- *          <SearchBox.Button />
- *        </SearchBox>
- *      </GridCol>
- *    </GridRow>
- *  </Layout>
+ * ```jsx
+ * import { SearchBox } from 'govuk-react';
+ *
+ * <SearchBox>
+ *   <SearchBox.Input placeholder="Search GOV.UK" />
+ *   <SearchBox.Button />
+ * </SearchBox>
  * ```
  *
  * ### References:
+ *
  * - https://govuk-static.herokuapp.com/component-guide/search
  *
  */
