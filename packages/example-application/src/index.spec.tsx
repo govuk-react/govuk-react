8e5edfb83315cb555b3333f4be712518c495f424
Alasdair McLeay <alasdair.mcleay@me.com>
2021-08-12 11:44:05 +0100

    React Hook Form - initial commit

--- packages/example-application/src/index.spec.tsx
+++ packages/example-application/src/index.spec.tsx
@@ -194,4 +194,68 @@ describe('When a user loads the application,', () => {
       });
     });
   });
+  // TODO: seperate specs
+  describe.only('browses to react hook form,', () => {
+    beforeEach(() => {
+      cy.contains('Menu').click();
+      cy.contains('React Hook Form').click();
+    });
+    describe('clicks submit,', () => {
+      beforeEach(() => {
+        cy.get('button').contains('Submit').click();
+      });
+      it('should show error messages', () => {
+        cy.contains('Error summary').should('be.visible');
+        // TODO: test that error summary items are anchor links to relevant field
+        cy.contains('Error summary').parent().contains('Please enter a first name').should('be.visible');
+        cy.contains('Error summary').parent().contains('Please enter a description').should('be.visible');
+        cy.contains('Error summary').parent().contains('Please select at least one nationality').should('be.visible');
+        cy.contains('Error summary').parent().contains('Please enter a date of birth').should('be.visible');
+        cy.contains('Error summary').parent().contains('Please select an animal').should('be.visible');
+        cy.contains('Error summary').parent().contains('Please answer the question').should('be.visible');
+
+        cy.get('[name="firstName"]').parent().contains('Please enter a first name').should('be.visible');
+        cy.get('[name="description"]').parent().contains('Please enter a description').should('be.visible');
+        cy.get('[name="nationality"]')
+          .parent()
+          .parent()
+          .contains('Please select at least one nationality')
+          .should('be.visible');
+        // TODO: better selector for date of birth
+        cy.contains('Date of birth').parent().contains('Please enter a date of birth').should('be.visible');
+        cy.get('[name="animal"]').parent().contains('Please select an animal').should('be.visible');
+        cy.get('[name="hasMultiplePets"]')
+          .parent()
+          .parent()
+          .contains('Please answer the question')
+          .should('be.visible');
+      });
+    });
+    describe('fills in all form fields,', () => {
+      beforeEach(() => {
+        cy.contains('First name').click().type('Mark');
+        cy.contains('Description of what you saw').click().type('Mark');
+        cy.contains('British').click();
+        cy.contains('Day').click().type('19');
+        cy.contains('Month').click().type('9');
+        cy.contains('Year').click().type('1999');
+        cy.contains('What animal is your pet').parent().find('select').select('Other feline');
+        cy.contains('Do you have more than one pet?').parent().contains('No').click();
+      });
+      describe('clicks submit,', () => {
+        beforeEach(() => {
+          cy.get('button').contains('Submit').click();
+        });
+        it('should show the confirmation screen', () => {
+          cy.contains('Application complete').should('be.visible');
+          cy.contains('Name: Mark').should('be.visible');
+          cy.contains('Description: Mark').should('be.visible');
+          cy.contains('Nationality: ["british"]').should('be.visible');
+          cy.contains('Date of birth: {"day":"19","month":"9","year":"1999"}').should('be.visible');
+          cy.contains('Animal: other-feline').should('be.visible');
+          cy.contains('Multiple pets: no').should('be.visible');
+        });
+      });
+    });
+  });
 });
---
+++ packages/example-application/src/react-hook-form.tsx
@@ -0,0 +1,120 @@
+/* eslint-disable react/prop-types */
+
+import React, { useState, useCallback } from 'react';
+import * as GovUK from 'govuk-react';
+import { Link } from 'react-router-dom';
+
+import { useForm } from 'react-hook-form';
+
+const ReactHookForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [data, setData] = useState(false);
+  const handleFormSubmit = useCallback(
+    (values) => {
+      if (isSubmitting) return;
+      setIsSubmitting(true);
+      setTimeout(() => {
+        setData(values);
+        setIsSubmitted(true);
+        setIsSubmitting(false);
+      }, 1000);
+    },
+    [isSubmitting]
+  );
+
+  const errorsToShow = Object.keys(errors);
+
+  return (
+    <>
+      {!isSubmitted && (
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <GovUK.LoadingBox loading={isSubmitting}>
+            <GovUK.BackLink as={Link} to="/">
+              Home
+            </GovUK.BackLink>
+            {!!errorsToShow?.length && (
+              <GovUK.ErrorSummary
+                heading="Error summary"
+                description="Please address the following issues"
+                errors={errorsToShow.map((key) => ({
+                  targetName: key,
+                  text: errors[key].message,
+                }))}
+              />
+            )}
+            <GovUK.Fieldset>
+              <GovUK.Fieldset.Legend size="M">About you</GovUK.Fieldset.Legend>
+              <GovUK.InputField
+                mb={4}
+                hint="You can find this on your passport"
+                {...register('firstName', {
+                  validate: (value) => (value ? undefined : 'Please enter a first name'),
+                })}
+              >
+                First name
+              </GovUK.InputField>
+              <GovUK.TextArea
+                mb={8}
+                hint="Enter as many words as you like"
+                {...register('description', {
+                  validate: (value) => (value ? undefined : 'Please enter a description'),
+                })}
+              >
+                Description of what you saw
+              </GovUK.TextArea>
+            </GovUK.Fieldset>
+            <GovUK.Fieldset>
+              <GovUK.Fieldset.Legend size="M">About your pet</GovUK.Fieldset.Legend>
+              <GovUK.Select
+                mb={8}
+                label="What animal is your pet"
+                hint="A cat for example"
+                {...register('animal', {
+                  validate: (value) => (value ? undefined : 'Please select an animal'),
+                })}
+              >
+                <option value="cat">Cat</option>
+                <option value="other-feline">Other feline</option>
+                <option value="other-non-feline">Other non feline</option>
+              </GovUK.Select>
+            </GovUK.Fieldset>
+            <GovUK.Button type="submit" disabled={isSubmitting}>
+              Submit
+            </GovUK.Button>
+          </GovUK.LoadingBox>
+        </form>
+      )}
+      {isSubmitted && (
+        <>
+          <GovUK.BackLink as={Link} to="/final-form" onClick={() => setIsSubmitted(false)}>
+            Back
+          </GovUK.BackLink>
+          <GovUK.Panel title="Application complete">Reference: XBR1N21R3</GovUK.Panel>
+          <GovUK.LeadParagraph>
+            Enim pariatur pariatur commodo incididunt ad nulla ex eu sunt ut ex id veniam veniam.
+          </GovUK.LeadParagraph>
+          <GovUK.Paragraph>
+            Consequat adipisicing aliquip eiusmod nostrud et proident non id consequat aliquip eiusmod aliquip.
+          </GovUK.Paragraph>
+          <GovUK.UnorderedList>
+            <GovUK.ListItem>Name: {data.firstName}</GovUK.ListItem>
+            <GovUK.ListItem>Description: {data.description}</GovUK.ListItem>
+            <GovUK.ListItem>Nationality: {JSON.stringify(data.nationality)}</GovUK.ListItem>
+            <GovUK.ListItem>Date of birth: {JSON.stringify(data.dob)}</GovUK.ListItem>
+            <GovUK.ListItem>Animal: {data.animal}</GovUK.ListItem>
+            <GovUK.ListItem>Multiple pets: {data.hasMultiplePets}</GovUK.ListItem>
+          </GovUK.UnorderedList>
+        </>
+      )}
+    </>
+  );
+};
+
+export default ReactHookForm;
