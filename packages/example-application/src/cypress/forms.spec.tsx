89c8307beef7cca9326c31f67734908def64d5aa
Alasdair McLeay <alasdair.mcleay@me.com>
2021-08-25 17:32:53 +0100

    Move cypress spec in to sub folder

--- packages/example-application/src/cypress/forms.spec.tsx
+++ packages/example-application/src/cypress/forms.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import App from './index';
+import App from '../index';
 
 describe('When a user loads the application,', () => {
   beforeEach(() => {
