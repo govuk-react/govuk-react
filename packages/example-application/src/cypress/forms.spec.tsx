ee75b30b35edf72b481853da5f7f21f37b7561eb
Alasdair McLeay <alasdair.mcleay@me.com>
2021-08-25 18:24:23 +0100

    Loop instead of abstract in cypress tests

--- packages/example-application/src/cypress/forms.spec.tsx
+++ packages/example-application/src/cypress/forms.spec.tsx
@@ -2,154 +2,73 @@ import React from 'react';
 import { mount } from '@cypress/react';
 import App from '../index';
 
-// we are abstracting some Cypress code as we want multiple implementations to exhibit identical behaviour
-const fillInForm = () => {
-  cy.contains('First name').click().type('Mark');
-  cy.contains('Description of what you saw').click().type('Mark');
-  cy.contains('British').click();
-  cy.contains('Day').click().type('19');
-  cy.contains('Month').click().type('9');
-  cy.contains('Year').click().type('1999');
-  cy.contains('What animal is your pet').parent().find('select').select('Other feline');
-  cy.contains('Do you have more than one pet?').parent().contains('No').click();
-};
-const allErrorMessagesShouldBeVisible = () => {
-  cy.contains('Error summary').should('be.visible');
-  // TODO: test that error summary items are anchor links to relevant field
-  cy.contains('Error summary').parent().contains('Please enter a first name').should('be.visible');
-  cy.contains('Error summary').parent().contains('Please enter a description').should('be.visible');
-  cy.contains('Error summary').parent().contains('Please select at least one nationality').should('be.visible');
-  cy.contains('Error summary').parent().contains('Please enter a date of birth').should('be.visible');
-  cy.contains('Error summary').parent().contains('Please select an animal').should('be.visible');
-  cy.contains('Error summary').parent().contains('Please answer the question').should('be.visible');
-
-  cy.get('[name="firstName"]').parent().contains('Please enter a first name').should('be.visible');
-  cy.get('[name="description"]').parent().contains('Please enter a description').should('be.visible');
-  cy.get('[name="nationality"]')
-    .parent()
-    .parent()
-    .contains('Please select at least one nationality')
-    .should('be.visible');
-  // TODO: better selector for date of birth
-  cy.contains('Date of birth').parent().contains('Please enter a date of birth').should('be.visible');
-  cy.get('[name="animal"]').parent().contains('Please select an animal').should('be.visible');
-  cy.get('[name="hasMultiplePets"]').parent().parent().contains('Please answer the question').should('be.visible');
-};
-const shouldShowConfirmation = () => {
-  cy.contains('Application complete').should('be.visible');
-  cy.contains('Name: Mark').should('be.visible');
-  cy.contains('Description: Mark').should('be.visible');
-  cy.contains('Nationality: ["british"]').should('be.visible');
-  cy.contains('Date of birth: {"day":"19","month":"9","year":"1999"}').should('be.visible');
-  cy.contains('Animal: other-feline').should('be.visible');
-  cy.contains('Multiple pets: no').should('be.visible');
-};
+const formModes = ['Basic', 'Final Form', 'Formik', 'React Hook Form'];
 
 describe('When a user loads the application, clicks Start now,', () => {
   beforeEach(() => {
     mount(<App />);
     cy.get('a').contains('Start now').click();
   });
-  describe('browses to the form,', () => {
-    beforeEach(() => {
-      cy.get('a').contains('Basic').click();
-    });
-    describe('clicks submit,', () => {
+  formModes.forEach((formMode) => {
+    describe(`browses to ${formMode},`, () => {
       beforeEach(() => {
-        cy.get('button').contains('Submit').click();
-      });
-      it('should show error messages', () => {
-        allErrorMessagesShouldBeVisible();
-      });
-    });
-    describe('fills in all form fields,', () => {
-      beforeEach(() => {
-        fillInForm();
+        cy.get('a').contains(formMode).click();
       });
       describe('clicks submit,', () => {
         beforeEach(() => {
           cy.get('button').contains('Submit').click();
         });
-        it('should show the confirmation screen', () => {
-          shouldShowConfirmation();
+        it('should show error messages', () => {
+          cy.contains('Error summary').should('be.visible');
+          // TODO: test that error summary items are anchor links to relevant field
+          cy.contains('Error summary').parent().contains('Please enter a first name').should('be.visible');
+          cy.contains('Error summary').parent().contains('Please enter a description').should('be.visible');
+          cy.contains('Error summary').parent().contains('Please select at least one nationality').should('be.visible');
+          cy.contains('Error summary').parent().contains('Please enter a date of birth').should('be.visible');
+          cy.contains('Error summary').parent().contains('Please select an animal').should('be.visible');
+          cy.contains('Error summary').parent().contains('Please answer the question').should('be.visible');
+
+          cy.get('[name="firstName"]').parent().contains('Please enter a first name').should('be.visible');
+          cy.get('[name="description"]').parent().contains('Please enter a description').should('be.visible');
+          cy.get('[name="nationality"]')
+            .parent()
+            .parent()
+            .contains('Please select at least one nationality')
+            .should('be.visible');
+          // TODO: better selector for date of birth
+          cy.contains('Date of birth').parent().contains('Please enter a date of birth').should('be.visible');
+          cy.get('[name="animal"]').parent().contains('Please select an animal').should('be.visible');
+          cy.get('[name="hasMultiplePets"]')
+            .parent()
+            .parent()
+            .contains('Please answer the question')
+            .should('be.visible');
         });
       });
-    });
-  });
-  describe('browses to final form,', () => {
-    beforeEach(() => {
-      cy.get('a').contains('Final Form').click();
-    });
-    describe('clicks submit,', () => {
-      beforeEach(() => {
-        cy.get('button').contains('Submit').click();
-      });
-      it('should show error messages', () => {
-        allErrorMessagesShouldBeVisible();
-      });
-    });
-    describe('fills in all form fields,', () => {
-      beforeEach(() => {
-        fillInForm();
-      });
-      describe('clicks submit,', () => {
+      describe('fills in all form fields,', () => {
         beforeEach(() => {
-          cy.get('button').contains('Submit').click();
+          cy.contains('First name').click().type('Mark');
+          cy.contains('Description of what you saw').click().type('Mark');
+          cy.contains('British').click();
+          cy.contains('Day').click().type('19');
+          cy.contains('Month').click().type('9');
+          cy.contains('Year').click().type('1999');
+          cy.contains('What animal is your pet').parent().find('select').select('Other feline');
+          cy.contains('Do you have more than one pet?').parent().contains('No').click();
         });
-        it('should show the confirmation screen', () => {
-          shouldShowConfirmation();
-        });
-      });
-    });
-  });
-  describe('browses to formik,', () => {
-    beforeEach(() => {
-      cy.get('a').contains('Formik').click();
-    });
-    describe('clicks submit,', () => {
-      beforeEach(() => {
-        cy.get('button').contains('Submit').click();
-      });
-      it('should show error messages', () => {
-        allErrorMessagesShouldBeVisible();
-      });
-    });
-    describe('fills in all form fields,', () => {
-      beforeEach(() => {
-        fillInForm();
-      });
-      describe('clicks submit,', () => {
-        beforeEach(() => {
-          cy.get('button').contains('Submit').click();
-        });
-        it('should show the confirmation screen', () => {
-          shouldShowConfirmation();
-        });
-      });
-    });
-  });
-  describe('browses to react hook form,', () => {
-    beforeEach(() => {
-      cy.get('a').contains('React Hook Form').click();
-    });
-    describe('clicks submit,', () => {
-      beforeEach(() => {
-        cy.get('button').contains('Submit').click();
-      });
-      it('should show error messages', () => {
-        allErrorMessagesShouldBeVisible();
-      });
-    });
-    describe('fills in all form fields,', () => {
-      beforeEach(() => {
-        fillInForm();
-      });
-      describe('clicks submit,', () => {
-        beforeEach(() => {
-          cy.get('button').contains('Submit').click();
-        });
-        it('should show the confirmation screen', () => {
-          shouldShowConfirmation();
+        describe('clicks submit,', () => {
+          beforeEach(() => {
+            cy.get('button').contains('Submit').click();
+          });
+          it('should show the confirmation screen', () => {
+            cy.contains('Application complete').should('be.visible');
+            cy.contains('Name: Mark').should('be.visible');
+            cy.contains('Description: Mark').should('be.visible');
+            cy.contains('Nationality: ["british"]').should('be.visible');
+            cy.contains('Date of birth: {"day":"19","month":"9","year":"1999"}').should('be.visible');
+            cy.contains('Animal: other-feline').should('be.visible');
+            cy.contains('Multiple pets: no').should('be.visible');
+          });
         });
       });
     });
