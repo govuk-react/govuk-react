c937c20fde701cc4190078f8227417dc876717bc
Alasdair McLeay <alasdair.mcleay@me.com>
2021-08-26 12:51:06 +0100

    Better validation message for pets

--- packages/example-application/src/cypress/forms.spec.tsx
+++ packages/example-application/src/cypress/forms.spec.tsx
@@ -26,7 +26,7 @@ describe('When a user loads the application, clicks Start now,', () => {
           cy.contains('Error summary').parent().contains('Please select at least one nationality').should('be.visible');
           cy.contains('Error summary').parent().contains('Please enter a date of birth').should('be.visible');
           cy.contains('Error summary').parent().contains('Please select an animal').should('be.visible');
-          cy.contains('Error summary').parent().contains('Please answer the question').should('be.visible');
+          cy.contains('Error summary').parent().contains('Please let us know if you have multiple pets').should('be.visible');
 
           cy.get('[name="firstName"]').parent().contains('Please enter a first name').should('be.visible');
           cy.get('[name="description"]').parent().contains('Please enter a description').should('be.visible');
@@ -40,7 +40,7 @@ describe('When a user loads the application, clicks Start now,', () => {
           cy.get('[name="hasMultiplePets"]')
             .parent()
             .parent()
-            .contains('Please answer the question')
+            .contains('Please let us know if you have multiple pets')
             .should('be.visible');
         });
       });
--- packages/example-application/src/forms/validators/validators.tsx
+++ packages/example-application/src/forms/validators/validators.tsx
@@ -1,5 +1,5 @@
 export const validateNationality = (value) => (value?.length ? undefined : 'Please select at least one nationality');
-export const validateMultiplePets = (value) => (value ? undefined : 'Please answer the question');
+export const validateMultiplePets = (value) => (value ? undefined : 'Please let us know if you have multiple pets');
 export const validateFirstName = (value) => (value ? undefined : 'Please enter a first name');
 export const validateDescription = (value) => (value ? undefined : 'Please enter a description');
 export const validateDateOfBirth = (value) => {
