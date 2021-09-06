63df6bf7783dd14c9d787e1d3a606cb734edb6be
Alasdair McLeay <penx@users.noreply.github.com>
2021-09-06 15:48:37 +0100

    Fix ts warnings in constants (#940)

--- packages/constants/src/test.tsx
+++ packages/constants/src/test.tsx
@@ -1,15 +1,15 @@
 // every export from constants should be a truthy (string, number, bool or plain non empty object)
 // no functions, no undefined
-import * as exports from '.';
+import * as constants from '.';
 
 const okTypes = ['number', 'string', 'boolean', 'object'];
 
 describe('Constants', () => {
-  for (let n = 0; n < Object.keys(exports).length; n += 1) {
-    const key = Object.keys(exports)[n];
+  for (let n = 0; n < Object.keys(constants).length; n += 1) {
+    const key = Object.keys(constants)[n];
 
     it(`exports ${key} as valid type`, () => {
-      const value = exports[key];
+      const value = constants[key];
       expect(okTypes.includes(typeof value)).toBe(true);
     });
   }
