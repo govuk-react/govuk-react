d8ac235d5715e89e2d2d8288f1d4c531a53102ef
Alasdair McLeay <alasdair.mcleay@me.com>
2021-09-06 13:48:13 +0100

    Fix ts issues in icons

--- packages/icons/src/SVGBase/index.tsx
+++ packages/icons/src/SVGBase/index.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { SVGProps as SVGPropsBase } from 'react';
 
-const SVG = ({ children, fill, title, ...rest }) => (
+export interface SVGProps extends SVGPropsBase<SVGSVGElement> {
+  children?: React.ReactNode;
+  title?: string;
+  fill?: string;
+  width?: string;
+}
+
+const SVG = ({ children = undefined, fill = 'currentColor', title = undefined, ...rest }: SVGProps) => (
   <svg version="1.1" fill={fill} width="100%" height="100%" style={{ display: 'block' }} {...rest}>
     <title>{title}</title>
     {children}
   </svg>
 );
-
-SVG.propTypes = {
-  children: PropTypes.node,
-  title: PropTypes.string,
-  fill: PropTypes.string,
-};
-
-SVG.defaultProps = {
-  children: undefined,
-  title: undefined,
-  fill: 'currentColor',
-};
 
 export default SVG;
