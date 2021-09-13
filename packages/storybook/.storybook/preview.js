07e3ab69e60830699b997cbb3e7e3d177eb4acfe
Alasdair McLeay <penx@users.noreply.github.com>
2021-09-13 20:30:49 +0100

    Disable Chromatic due to usage issues (#974)

--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -1,6 +1,7 @@
 export const parameters = {
   chromatic: {
     viewports: [320, 1200],
+    disableSnapshot: true // while we have usage issues we are disabling everthing in order to enable incrementally 
   },
   options: {
     storySort: {
