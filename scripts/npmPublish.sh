276f84819979efb678c1247d9b81383ab8a20540
Alasdair McLeay <alasdair.mcleay@me.com>
2021-09-13 20:22:29 +0100

    don't use yarn in npm publish script
    # Conflicts:
    #	scripts/npmPublish.sh

--- scripts/npmPublish.sh
+++ scripts/npmPublish.sh
@@ -2,7 +2,7 @@
 # detect pre-release based on version including a '-' character
 PRERELEASE=$(node -e 'console.log(require("package.json").version.split("-").length > 1)')
 if [ "$PRERELEASE" = "true" ]; then
-  yarn can-npm-publish --verbose && npm publish --tag next || true
+  npm exec can-npm-publish --verbose && npm publish --tag next || true
 else
-  yarn can-npm-publish --verbose && npm publish || true
+  npm exec can-npm-publish --verbose && npm publish || true
 fi
