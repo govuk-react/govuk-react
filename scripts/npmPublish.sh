2f1926ebbe967cb06b834b0b2eb2853e9d95aa61
Alasdair McLeay <penx@users.noreply.github.com>
2021-09-13 21:34:21 +0100

    Use npm workspaces (#850)

--- scripts/npmPublish.sh
+++ scripts/npmPublish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # detect pre-release based on version including a '-' character
-PRERELEASE=$(node -e 'console.log(require(`${process.env.LERNA_ROOT_PATH}/lerna.json`).version.split("-").length > 1)')
+PRERELEASE=$(node -e 'console.log(require("package.json").version.split("-").length > 1)')
 if [ "$PRERELEASE" = "true" ]; then
-  yarn can-npm-publish --verbose && npm publish --tag next || true
+  npm exec can-npm-publish --verbose && npm publish --tag next || true
 else
-  yarn can-npm-publish --verbose && npm publish || true
+  npm exec can-npm-publish --verbose && npm publish || true
 fi
