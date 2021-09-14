44ee09b53f3a82b468c3eae0933c1a95ef9f3a93
Alasdair McLeay <alasdair.mcleay@me.com>
2021-09-14 15:07:02 +0100

    Fix storybook build

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
     '../src/*.stories.mdx',
     '../../../packages/example-application/src/**/?(*.)stories.[jt]s?(x)',
     '../../../components/*/src/**/?(*.)stories.[jt]s?(x)',
-    '../../../packages/hoc/src/**/?(*.)stories.[jt]s?(x)',
     '../../../packages/icons/src/**/?(*.)stories.[jt]s?(x)',
   ],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs', '@storybook/addon-docs'],
