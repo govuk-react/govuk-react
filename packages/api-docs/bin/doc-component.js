c540de468c868ed977a00a33bf0da7b2d2fa4ff1
Alasdair McLeay <alasdair.mcleay@me.com>
2021-09-27 21:28:51 +0100

    Fix doc component

--- packages/api-docs/bin/doc-component.js
+++ packages/api-docs/bin/doc-component.js
@@ -2,7 +2,7 @@
 
 const program = require('commander');
 const { version } = require('../package.json');
-const doc = require('../lib');
+const doc = require('../dist/govuk-react-api-docs.cjs.prod.js');
 
 program.version(version).usage('<glob-to-components> <markdown-file>');
 
@@ -11,4 +11,4 @@ program.parse(process.argv);
 const glob = program.args[0];
 const markdown = program.args[1];
 
-doc(glob, markdown);
+doc.default(glob, markdown);
