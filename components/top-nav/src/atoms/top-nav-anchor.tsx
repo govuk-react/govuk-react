5fb6ad142d54f20054d86b86c9514b2f9a600eec
Alasdair McLeay <alasdair.mcleay@me.com>
2021-09-29 10:40:49 +0100

    TODO replaced with issue #1003

--- components/top-nav/src/atoms/top-nav-anchor.tsx
+++ components/top-nav/src/atoms/top-nav-anchor.tsx
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 import { WHITE, YELLOW } from 'govuk-colours';
 
 const TopNavAnchor = styled('a')({
-  color: WHITE, // TODO: active state, LIGHT_BLUE
+  color: WHITE,
   textDecoration: 'none',
   textDecorationSkipInk: 'none',
   borderBottom: '1px solid transparent',
   fontWeight: 700,
   lineHeight: 1,
   ':hover': {
-    borderBottomColor: WHITE, // TODO: active state, LIGHT_BLUE
+    borderBottomColor: WHITE,
   },
   ':focus': {
     outline: `3px solid ${YELLOW}`,
