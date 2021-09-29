e35053594850db4263663f734fd36a1fee73dda6
Alasdair McLeay <alasdair.mcleay@me.com>
2021-09-29 10:48:05 +0100

    TODO replaced with issue #851

--- packages/constants/src/index.tsx
+++ packages/constants/src/index.tsx
@@ -53,7 +53,6 @@ export const MEDIA_QUERIES: {
   DESKTOP: `@media only screen and (min-width: ${BREAKPOINTS.DESKTOP})`,
 };
 
-// TODO: figure out how to optionally include locally installed font, e.g. "GDS Transport Website"
 export const NTA_LIGHT = '"nta", Arial, sans-serif';
 export const NTA_LIGHT_TABULAR = `"ntatabularnumbers", ${NTA_LIGHT}`;
 export const FONT_STACK = `${NTA_LIGHT}`;
