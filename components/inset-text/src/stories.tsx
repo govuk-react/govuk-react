1ce92b5fefd818fc6a3bc2ffc7d2ca61efe7bbf2
Alasdair McLeay <alasdair.mcleay@me.com>
2021-09-30 09:18:23 +0100

    InsetText

--- components/inset-text/src/stories.tsx
+++ components/inset-text/src/stories.tsx
@@ -1,9 +1,30 @@
+import type { Story } from '@storybook/react';
+
+import React from 'react';
+import Paragraph from '@govuk-react/paragraph';
+
 import { InsetText } from '.';
-import InsetTextExample, { InsetTextWithParagraph } from './fixtures';
 
 export default {
   title: 'Typography/Inset text',
   component: InsetText,
+};
+
+export const testText =
+  'It can take up to 8 weeks to register a lasting power of attorney if there are no mistakes in the application.';
+
+const InsetTextExample: Story = (args) => <InsetText {...args} />;
+InsetTextExample.args = {
+  children: testText,
+};
+
+export const InsetTextWithParagraph: Story = ({ children }: { children: string }) => (
+  <InsetText>
+    <Paragraph mb={0}>{children}</Paragraph>
+  </InsetText>
+);
+InsetTextWithParagraph.args = {
+  children: testText,
 };
 
 export const Default = InsetTextExample;
--- components/inset-text/src/test.tsx
+++ components/inset-text/src/test.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import Paragraph from '@govuk-react/paragraph';
 
-import InsetTextTest, { InsetTextWithParagraph, testText } from './fixtures';
+import { InsetText } from '.';
 
 describe('inset text', () => {
   it('renders provided text', () => {
-    const wrapper = mount(<InsetTextTest />);
+    const wrapper = mount(
+      <InsetText>
+        It can take up to 8 weeks to register a lasting power of attorney if there are no mistakes in the application.
+      </InsetText>
+    );
 
-    expect(wrapper.text()).toContain(testText);
+    expect(wrapper.text()).toContain(
+      'It can take up to 8 weeks to register a lasting power of attorney if there are no mistakes in the application.'
+    );
   });
 
   it('matches the InsetText snapshot', () => {
-    expect(mount(<InsetTextTest />)).toMatchSnapshot('inset text');
+    expect(
+      mount(
+        <InsetText>
+          It can take up to 8 weeks to register a lasting power of attorney if there are no mistakes in the application.
+        </InsetText>
+      )
+    ).toMatchSnapshot('inset text');
   });
 
   it('can render with a paragraph', () => {
-    expect(mount(<InsetTextWithParagraph />)).toMatchSnapshot('inset text with paragraph');
+    expect(
+      mount(
+        <InsetText>
+          <Paragraph mb={0}>
+            It can take up to 8 weeks to register a lasting power of attorney if there are no mistakes in the
+            application.
+          </Paragraph>
+        </InsetText>
+      )
+    ).toMatchSnapshot('inset text with paragraph');
   });
 });
