3569b9c15c1382528ac9d731136543fa943a3cb2
Alasdair McLeay <alasdair.mcleay@me.com>
2021-10-04 14:03:30 +0100

    Label

--- components/label/src/stories.tsx
+++ components/label/src/stories.tsx
@@ -1,5 +1,8 @@
+import type { Story } from '@storybook/react';
+
 import * as React from 'react';
 
+import { LabelText, HintText, ErrorText, Input } from 'govuk-react';
 import { Label } from '.';
 
 export default {
@@ -8,4 +11,21 @@ export default {
   component: Label,
 };
 
-export const Default: React.FC = () => <Label>Example</Label>;
+const Template: Story<React.ComponentProps<typeof Label>> = (args) => <Label {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: 'Example',
+};
+
+export const WithInput = Template.bind({});
+
+WithInput.args = {
+  children: [
+    <LabelText>Example label</LabelText>,
+    <HintText>Example hint</HintText>,
+    <ErrorText>Example error</ErrorText>,
+    <Input />,
+  ],
+};
