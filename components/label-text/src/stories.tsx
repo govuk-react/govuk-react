622c0e991298ddfed6d3f4bda282e6eeafd7ad36
Alasdair McLeay <alasdair.mcleay@me.com>
2021-10-04 14:16:50 +0100

    LabelText

--- components/label-text/src/stories.tsx
+++ components/label-text/src/stories.tsx
@@ -1,4 +1,7 @@
+import type { Story } from '@storybook/react';
+
 import * as React from 'react';
+import { Label, HintText, ErrorText, Input } from 'govuk-react';
 
 import { LabelText } from '.';
 
@@ -8,4 +11,26 @@ export default {
   component: LabelText,
 };
 
-export const Default: React.FC = () => <LabelText>Example</LabelText>;
+const Template: Story<React.ComponentProps<typeof LabelText>> = (args) => <LabelText {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: 'Example',
+};
+
+export const WithWrappingLabel = Template.bind({});
+WithWrappingLabel.decorators = [
+  (StoryFunc) => (
+    <Label>
+      {StoryFunc()}
+      <HintText>Example hint</HintText>
+      <ErrorText>Example error</ErrorText>
+      <Input />
+    </Label>
+  ),
+];
+
+WithWrappingLabel.args = {
+  children: 'Example',
+};
