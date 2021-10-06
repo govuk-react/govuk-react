9d00c4f6b28016374a2f3a42df190d294e0009b3
Alasdair McLeay <alasdair.mcleay@me.com>
2021-10-06 13:54:03 +0100

    Select

--- components/select/src/stories.tsx
+++ components/select/src/stories.tsx
@@ -1,12 +1,11 @@
+import type { Story } from '@storybook/react';
+
 import * as React from 'react';
+import { action } from '@storybook/addon-actions';
 
 import LabelText from '@govuk-react/label-text';
-import { Select, SelectInput } from '.';
 
-const meta = {
-  touched: true,
-  error: 'Example',
-};
+import { Select, SelectInput } from '.';
 
 export default {
   title: 'Form/Select',
@@ -14,45 +13,52 @@ export default {
   component: Select,
 };
 
-// TODO: add a synthetic onChange event
-export const Default: React.FC = () => (
-  <Select input={{ name: 'group1' }} label="This is a label">
-    <option value="0">GOV.UK elements option 1</option>
-    <option value="1">GOV.UK elements option 2</option>
-    <option value="2">GOV.UK elements option 3</option>
-  </Select>
-);
+const Template: Story<React.ComponentProps<typeof Select>> = (args) => <Select {...args} />;
 
-export const SelectWithHintText: React.FC = () => (
-  <Select
-    input={{ name: 'group1' }}
-    label="This is a label"
-    hint={['This is and example of hintText/description of what we need from you.']}
-  >
-    <option value="0">GOV.UK elements option 1</option>
-    <option value="1">GOV.UK elements option 2</option>
-    <option value="2">GOV.UK elements option 3</option>
-  </Select>
-);
+export const Default = Template.bind({});
+Default.args = {
+  input: { name: 'group1', onChange: action('onChange') },
+  label: 'This is a label',
+  children: [
+    <option value="0">GOV.UK elements option 1</option>,
+    <option value="1">GOV.UK elements option 2</option>,
+    <option value="2">GOV.UK elements option 3</option>,
+  ],
+};
 
-export const SelectWithHintTextError: React.FC = () => (
-  <Select
-    input={{ name: 'group1' }}
-    label="This is a label"
-    hint={['This is and example of hintText/description of what we need from you.']}
-    meta={meta}
-  >
-    <option value="0">GOV.UK elements option 1</option>
-    <option value="1">GOV.UK elements option 2</option>
-    <option value="2">GOV.UK elements option 3</option>
-  </Select>
-);
+export const SelectWithHintText = Template.bind({});
+SelectWithHintText.args = {
+  input: { name: 'group1', onChange: action('onChange') },
+  label: 'This is a label',
+  hint: 'This is and example of hintText/description of what we need from you.',
+  children: [
+    <option value="0">GOV.UK elements option 1</option>,
+    <option value="1">GOV.UK elements option 2</option>,
+    <option value="2">GOV.UK elements option 3</option>,
+  ],
+};
 
-export const StandaloneInputWithInlineLabel: React.FC = () => (
+export const SelectWithHintTextError = Template.bind({});
+SelectWithHintTextError.args = {
+  input: { name: 'group1', onChange: action('onChange') },
+  label: 'This is a label',
+  hint: 'This is and example of hintText/description of what we need from you.',
+  meta: {
+    touched: true,
+    error: 'Example',
+  },
+  children: [
+    <option value="0">GOV.UK elements option 1</option>,
+    <option value="1">GOV.UK elements option 2</option>,
+    <option value="2">GOV.UK elements option 3</option>,
+  ],
+};
+
+export const StandaloneInputWithInlineLabel: Story = (args) => (
   <label>
     <LabelText>
       Sort by:&nbsp;
-      <SelectInput>
+      <SelectInput {...args}>
         <option value="0">People</option>
         <option value="1">Animals</option>
         <option value="2">Vegetables</option>
