ad1f147b4a7aa79230c125f8155df8d5e766bc35
David Winter <i@djw.me>
2021-11-10 12:40:14 +0000

    Set storybook to be based on light theme (#1048)
    If your OS (such as macOS) is set to use the dark theme, some browsers pass this through to Storybook which will then use the dark theme as default. This means components will then not have a white background when viewing within Storybook.
    
    This commit set the base theme to be the 'light' Storybook theme, regardless of what your OS setting is, as the components are meant to be placed on a white background.

--- packages/storybook/.storybook/manager.js
+++ packages/storybook/.storybook/manager.js
@@ -3,6 +3,7 @@ import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming';
 
 const theme = create({
+  base: 'light',
   brandTitle: 'govuk-react',
   sortStoriesByKind: true,
   brandUrl: 'https://github.com/govuk-react/govuk-react',
