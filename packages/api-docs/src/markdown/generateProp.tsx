d06754c040910ca4872683b3cb7b11ec4bd34f06
Adam Jones <domdomegg+git@gmail.com>
2022-04-30 17:46:23 +0100

    Improve doc generation to escape union types in markdown tables

--- packages/api-docs/src/markdown/generateProp.tsx
+++ packages/api-docs/src/markdown/generateProp.tsx
@@ -1,12 +1,13 @@
 import generatePropType from './generatePropType';
 import generatePropDefaultValue from './generatePropDefaultValue';
 import generatePropDescription from './generatePropDescription';
+import escapeForTable from './escapeForTable';
 
 export default function generateProp(propName, prop) {
-  const defaultValue = prop.defaultValue ? generatePropDefaultValue(prop.defaultValue) : '';
+  const defaultValue = prop.defaultValue ? `\`\`\`${generatePropDefaultValue(prop.defaultValue)}\`\`\`` : '';
   const propType = prop.type ? generatePropType(prop.type) : '';
   const description = prop.description ? generatePropDescription(prop.description) : '';
-  return ` \`${propName}\` | ${
-    prop.required ? 'true' : ''
-  } | \`\`\`${defaultValue}\`\`\` | ${propType} | ${description}`;
+  return ` \`${escapeForTable(propName)}\` | ${prop.required ? 'true' : ''} | ${defaultValue} | ${escapeForTable(
+    propType
+  )} | ${escapeForTable(description)}`;
 }
