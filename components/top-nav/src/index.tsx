92eb33730c42fdb55441fc2f9ab8ff72a4039c46
Jas Aggarwal <webmaster@jaspaul.com>
2023-01-23 16:48:00 +0000

    snapshot update

--- components/top-nav/src/index.tsx
+++ components/top-nav/src/index.tsx
@@ -84,7 +84,7 @@ export class TopNav extends Component<TopNavProps, { navigationOpen: boolean }> 
 
   static displayName = 'TopNav';
 
-  static defaultProps = {
+  static defaultProps: TopNavProps = {
     bgColor: BLACK,
     color: WHITE,
     company: <IconTitle icon={<CrownIcon width="36" height="32" />}>GOV.UK</IconTitle>,
