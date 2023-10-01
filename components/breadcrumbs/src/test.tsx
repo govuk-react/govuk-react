b463431f578da5d27e0864dee841d2e33fe8f9dc
Alasdair McLeay <penx@users.noreply.github.com>
2023-10-02 00:35:57 +0100

    Remove enzyme, snapshots (#1267)
    - ReactDOM.render is deprecated
    - Snapshot testing has not proven to catch issues
    - Visual regression is preferred over snapshots
    - "renders without crashing" tests covered by storybook + chromatic
    - testing library preferred over enzyme
    - don't want tests that test for implementation details

--- components/breadcrumbs/src/test.tsx
+++ components/breadcrumbs/src/test.tsx
@@ -1,28 +1,28 @@
 import * as React from 'react';
-import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
 
 import { Breadcrumbs } from '.';
 
 describe('breadcrumbs', () => {
-  it('should render an item in ordered list', () => {
-    const wrapper = mount(<Breadcrumbs>example</Breadcrumbs>);
+  it('should render an item in ordered list', async () => {
+    const wrapper = render(<Breadcrumbs>example</Breadcrumbs>);
 
-    expect(wrapper.find('ol')).toHaveLength(1);
+    expect(await wrapper.findAllByRole('list')).toHaveLength(1);
   });
 
-  it('should render multiple items in ordered list', () => {
-    const wrapper = mount(
+  it('should render multiple items in ordered list', async () => {
+    const wrapper = render(
       <Breadcrumbs>
         <Breadcrumbs.Link href="/section">Section 1</Breadcrumbs.Link>
         example
       </Breadcrumbs>
     );
 
-    expect(wrapper.find('ol li')).toHaveLength(2);
+    expect(await wrapper.findAllByRole('listitem')).toHaveLength(2);
   });
 
-  it('should render an ordered list without ghost/duff children', () => {
-    const wrapper = mount(
+  it('should render an ordered list without ghost/duff children', async () => {
+    const wrapper = render(
       <Breadcrumbs>
         <Breadcrumbs.Link href="/section">Section 1</Breadcrumbs.Link>
         {[] /* empty node */}
@@ -31,28 +31,12 @@ describe('breadcrumbs', () => {
       </Breadcrumbs>
     );
 
-    expect(wrapper.find('ol li')).toHaveLength(2);
+    expect(await wrapper.findAllByRole('listitem')).toHaveLength(2);
   });
 
-  it('allows attributes to be set', () => {
-    const wrapper = mount(
-      <Breadcrumbs id="test" className="test">
-        Crumb
-      </Breadcrumbs>
-    );
+  it('allows attributes to be set', async () => {
+    const wrapper = render(<Breadcrumbs data-testid="test">Crumb</Breadcrumbs>);
 
-    expect(wrapper.hasClass('test')).toBe(true);
-    expect(wrapper.is('#test')).toBe(true);
-  });
-
-  it('matches snapshot', () => {
-    const wrapper = mount(
-      <Breadcrumbs>
-        <Breadcrumbs.Link href="/section">Section 1</Breadcrumbs.Link>
-        <Breadcrumbs.Link href="/section2">Section 2</Breadcrumbs.Link>
-      </Breadcrumbs>
-    );
-
-    expect(wrapper).toMatchSnapshot('breadcrumbs');
+    expect(await wrapper.getByTestId('test')).toBeInTheDocument();
   });
 });
